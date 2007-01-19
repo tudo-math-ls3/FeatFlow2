404c1dd70baf45424f8af1c8ccb8ec0d81feb1a9
Michael Koester <michael.koester@math.tu-dortmund.de>
2007-01-19 12:57:01 +0000

    New features        : Added heatcond example: Nonstationary heat conduction equation. Problems solved     : Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@504 4b12f95d-9313-0410-afc9-bd26427041f1

---
+++ applications/heatconduction/src/heatcond.f90
@@ -0,0 +1,76 @@
+!##############################################################################
+!# ****************************************************************************
+!# <name> heatcond </name>
+!# ****************************************************************************
+!#
+!# <purpose>
+!# This program is a simple test program for discretising the equation
+!#
+!#   d/dt u  - div ( alpha*grad(u) )  +  beta*grad(u)  +  gamma*u  =  f
+!#
+!# on a 2D domain for a scalar function u=u(x,t).
+!#
+!# The main example (module heatcond_method5) separates the different stages
+!# of the solution process into different subroutines.
+!# The communication is done using a problem-related structure. For the
+!# communication with callback routines during the assembly, a
+!# collection structure is set up. As solver, a simple Multigrid solver is
+!# used.
+!#
+!# The equation itself is set up using parameters of a .DAT file
+!# named 'data/heatcond.dat'. Here all parameters (alpha, beta, gamma, level,...)
+!# can be found and changed by the user without recompiling the program.
+!# </purpose>
+!##############################################################################
+
+PROGRAM heatcond
+
+  USE heatcond_method5
+  
+  IMPLICIT NONE
+  
+  INCLUDE 'cmem.inc'
+  INCLUDE 'cout.inc'
+  INCLUDE 'cerr.inc'
+  INCLUDE 'cfileout.inc'
+
+  ! As we still use some FEAT 1.x routines, we have to initialise some
+  ! output variables.
+
+  M = 0
+  MT = 2
+  MTERM = 6
+  MFILE = 0
+  ICHECK = 0
+
+  ! The very first thing in every application: 
+  ! Initialise system-wide settings:
+  
+  CALL system_init()
+
+  ! The very second thing in every program: 
+  ! Initialise the storage management: 
+  !
+  ! 2.) Initialise FEAT 2.0 storage management:
+  CALL storage_init(999, 100)
+
+  ! 3.) Initialise old FEAT 1.x storage management for compatibility.
+  CALL ZINIT(NNWORK,'feat.msg','log/feat1.err','log/feat1.prt',&
+             'log/feat1.sys','log/feat1.trc') 
+  
+  ! Call the problem to solve - method 5 = multigrid
+  PRINT *
+  PRINT *,'Calculating heatcond-Problem with method 5'
+  PRINT *,'------------------------------------------'
+  CALL heatcond5
+  
+  ! Print out heap statistics - just to check if everything
+  ! is cleaned up.
+  ! This should display 'Handles in use=0' and 'Memory in use=0'!
+  PRINT *
+  CALL storage_info(.TRUE.)
+  
+  ! Clean up the storage management, finish
+  CALL storage_done()
+  
+END PROGRAM
