0a033192898d75d581a323ad2b014c0546a67a49
Michael Koester <michael.koester@math.tu-dortmund.de>
2007-03-21 16:02:50 +0000

    New features        : Problems solved     : Reverted the changes; didn't work. Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@665 4b12f95d-9313-0410-afc9-bd26427041f1

--- kernel/System/afcutil.f90
+++ kernel/System/afcutil.f90
@@ -31,7 +31,7 @@ CONTAINS
 
   FUNCTION feat_htpint(n,handle) RESULT(p)
     INTEGER, INTENT(IN) :: n,handle
-    INTEGER(I32), DIMENSION(:), POINTER :: p
+    INTEGER, DIMENSION(:), POINTER :: p
     INCLUDE 'cmem.inc'
     
     p => feat_vi(n,KWORK(L(handle)))
@@ -40,7 +40,7 @@ CONTAINS
 
   FUNCTION feat_htpint2D(n,m,handle) RESULT(p)
     INTEGER, INTENT(IN) :: n,m,handle
-    INTEGER(I32), DIMENSION(:,:), POINTER :: p
+    INTEGER, DIMENSION(:,:), POINTER :: p
     INCLUDE 'cmem.inc'
     
     p => feat_mi(n,m,KWORK(L(handle)))
@@ -68,8 +68,8 @@ CONTAINS
 
   FUNCTION feat_vi(n,arr) RESULT(p)
     INTEGER, INTENT(IN) :: n
-    INTEGER(I32), DIMENSION(n), TARGET :: arr
-    INTEGER(I32), DIMENSION(:), POINTER :: p
+    INTEGER, DIMENSION(n), TARGET :: arr
+    INTEGER, DIMENSION(:), POINTER :: p
 
     p => arr
 
@@ -77,8 +77,8 @@ CONTAINS
 
   FUNCTION feat_mi(n,m,arr) RESULT(p)
     INTEGER, INTENT(IN) :: n,m
-    INTEGER(I32), DIMENSION(n,m), TARGET :: arr
-    INTEGER(I32), DIMENSION(:,:), POINTER :: p
+    INTEGER, DIMENSION(n,m), TARGET :: arr
+    INTEGER, DIMENSION(:,:), POINTER :: p
 
     p => arr
 
