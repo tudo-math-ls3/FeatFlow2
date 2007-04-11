5dfb25467009ef06b27df302fd9d9ee8d3ef7249
Michael Koester <michael.koester@math.tu-dortmund.de>
2007-04-11 09:24:12 +0000

    New features        : Peter Zajac: - added bgeom_transformPoint2D - added bgeom_transformBackPoint2D Problems solved     : Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@744 4b12f95d-9313-0410-afc9-bd26427041f1

--- kernel/BasicGeometry/basicgeometry.f90
+++ kernel/BasicGeometry/basicgeometry.f90
@@ -6,6 +6,16 @@
 !# <purpose>
 !# This module contains the basic entity of the whole FEAT world: a point,
 !# a coordinate system and a set of routines to maintain that.
+!#
+!# The following routines can be found in this module:
+!#
+!# 1.) bgeom_transformPoint2D
+!#     -> Transforms a 2D point with coordinates relative to a given coordinate
+!#        system to world coordinates.
+!#
+!# 2.) bgeom_transformBackPoint2D
+!#     -> Transforms a 2D point with world coordinates to coordinates relative
+!#        to a given coordinate system.
 !# </purpose>
 !##############################################################################
 
@@ -17,7 +27,7 @@ MODULE basicgeometry
 
 
   ! One could ask: Why do we need the following two constants...?
-  ! The answer is simple: Then we can more easily GREP for dimsnion-dependent
+  ! The answer is simple: Then we can more easily GREP for dimension-dependent
   ! quantities :-)
   
 !<constants>
@@ -95,5 +105,133 @@ MODULE basicgeometry
 
   !</typeblock>
   !</types>
+
+CONTAINS
+
+  ! ***************************************************************************
+  
+!<subroutine>
+
+  PURE SUBROUTINE bgeom_transformPoint2D(rcoordSys, DpointIn, DpointOut)
+
+!<description>
+  ! Transforms a given 2D point with coordinates relative to the given
+  ! coordinate system to world coordinates.
+!</description>
+
+!<input>
+  ! The 2D coordinate system that is to be applied to the given point.
+  TYPE(t_coordinateSystem2D),  INTENT(IN)  :: rcoordSys
+
+  ! The 2D point that is to be transformed.
+  REAL(DP), DIMENSION(:),      INTENT(IN)  :: DpointIn
+!</input>
+
+!<output>
+  ! The transformed 2D point.
+  REAL(DP), DIMENSION(:),      INTENT(OUT) :: DpointOut
+!</output>
+
+!</subroutine>
+
+    ! Check if the rotation of the coordinate system is non-zero.
+    IF (rcoordSys%drotation .NE. 0.0_DP) THEN
+
+      ! Apply rotation to the input coordinates.
+      DpointOut(1) = (rcoordSys%dcos_rotation * DpointIn(1)) - &
+                     (rcoordSys%dsin_rotation * DpointIn(2))
+      DpointOut(2) = (rcoordSys%dsin_rotation * DpointIn(1)) + &
+                     (rcoordSys%dcos_rotation * DpointIn(2))
+
+    ELSE
+
+      ! No rotation in the coordinate system, so simply copy the input coords.
+      DpointOut(1) = DpointIn(1)
+      DpointOut(2) = DpointIn(2)
+
+    END IF
+
+    ! Now scale the given coordinates by the scaling factor and translate them
+    ! by the origin of our coordinate system.
+    DpointOut(1) = (rcoordSys%dscalingFactor * DpointOut(1)) + &
+                    rcoordSys%Dorigin(1)
+    DpointOut(2) = (rcoordSys%dscalingFactor * DpointOut(2)) + &
+                    rcoordSys%Dorigin(2)
+
+    ! That's it!
+
+  END SUBROUTINE
+
+
+  ! ***************************************************************************
+  
+!<subroutine>
+
+  PURE SUBROUTINE bgeom_transformBackPoint2D(rcoordSys, DpointIn, DpointOut)
+
+!<description>
+  ! Transform a 2D point given in world coordinates to coordinates relative
+  ! to a given 2D coordinate system.
+  ! This subroutine is the inverse routine of bgeom_transformPoint2D.
+!</description>
+
+!<input>
+  ! The 2D coordinate system of our input point.
+  TYPE(t_coordinateSystem2D), INTENT(IN)  :: rcoordSys
+
+  ! The 2D point that is to be transformed, relative to the given coordinate
+  ! system.
+  REAL(DP), DIMENSION(:),     INTENT(IN)  :: DpointIn
+!</input>
+
+!<output>
+  ! The transformed 2D point, in world coordinates.
+  REAL(DP), DIMENSION(:),     INTENT(OUT) :: DpointOut
+!</output>
+
+!</subroutine>
+
+    ! local variables
+    REAL(DP) :: X,Y
+
+    ! Translate the given point by the negatives of our coordinate system
+    ! origin.
+    X = DpointIn(1) - rcoordSys%Dorigin(1)
+    Y = DpointIn(2) - rcoordSys%Dorigin(2)
+
+    ! Now scale the point by the inverse of our coordinate system sclaing
+    ! factor, of course only if it is non-zero ...
+    IF (rcoordSys%dscalingFactor .NE. 0.0_DP) THEN
+
+      X = X / rcoordSys%dscalingFactor
+      Y = Y / rcoordSys%dscalingFactor
+
+    ELSE
+
+      ! ... or set the result to zero and exit subroutine otherwise.
+      DpointOut(1) = 0.0_DP
+      DpointOut(2) = 0.0_DP
+      RETURN
+
+    ENDIF
+
+    ! Check if the rotation of the coordinate system is non-zero.
+    IF (rcoordSys%drotation .NE. 0.0_DP) THEN
+
+      ! Apply rotation to the input coordinates.
+      DpointOut(1) = ( rcoordSys%dcos_rotation * X) + (rcoordSys%dsin_rotation * Y)
+      DpointOut(2) = (-rcoordSys%dsin_rotation * X) + (rcoordSys%dcos_rotation * Y)
+
+    ELSE
+
+      ! No rotation in the coordinate system, so simply copy the input coords.
+      DpointOut(1) = X
+      DpointOut(2) = Y
+
+    END IF
+
+    ! That's it
+
+  END SUBROUTINE
 
 END MODULE
