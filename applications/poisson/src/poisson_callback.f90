5471ecc664239aa50b8b65fbbaf28eae1b723854
Michael Koester <michael.koester@math.tu-dortmund.de>
2007-04-17 15:12:37 +0000

    New features        : - Added a comment how to get X/Y coordinates in the getBoundaryValues callback function. Problems solved     : Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@804 4b12f95d-9313-0410-afc9-bd26427041f1

--- applications/poisson/src/poisson_callback.f90
+++ applications/poisson/src/poisson_callback.f90
@@ -405,7 +405,13 @@ CONTAINS
   
 !</subroutine>
 
-  REAL(DP) :: dx,dy
+
+  ! To get the X/Y-coordinates of the boundary point, use:
+  !
+  ! REAL(DP) :: dx,dy
+  !
+  ! CALL boundary_getCoords(rdiscretisation%p_rboundary, &
+  !     rbcRegion%rboundaryRegion%iboundCompIdx, dwhere, dx, dy)
 
   ! Return zero Dirichlet boundary values for all situations.
   Dvalues(1) = 0.0_DP
