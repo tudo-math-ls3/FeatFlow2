af9145070d704e7c10e23bc53644fe67a4ecccc2
Michael Koester <michael.koester@math.tu-dortmund.de>
2007-04-30 10:11:48 +0000

    New features        : - After Rename DcornerCoordinates in DvertexCoors. Problems solved     : Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@965 4b12f95d-9313-0410-afc9-bd26427041f1

--- applications/poisson/src/poisson_callback.f90
+++ applications/poisson/src/poisson_callback.f90
@@ -513,7 +513,7 @@ CONTAINS
       
       ! Get the triangulation array for the point coordinates
       p_rtriangulation => rdiscretisation%RspatialDiscretisation(1)%p_rtriangulation
-      CALL storage_getbase_double2d (p_rtriangulation%h_DcornerCoordinates,&
+      CALL storage_getbase_double2d (p_rtriangulation%h_DvertexCoords,&
                                      p_DvertexCoordinates)
 
       ! Definition of the circle
