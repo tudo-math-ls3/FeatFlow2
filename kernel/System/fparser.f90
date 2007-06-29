e0fb7014ac55a59e1fd5c0d62749b4d0789d3d36
Michael Koester <michael.koester@math.tu-dortmund.de>
2007-06-29 11:53:34 +0000

    New features        : Problems solved     : - Removed debug output of bytecode. Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@1590 4b12f95d-9313-0410-afc9-bd26427041f1

--- kernel/System/fparser.f90
+++ kernel/System/fparser.f90
@@ -898,7 +898,7 @@ CONTAINS
 
     CALL Compile (rparser%Comp(iComp),Func,Var)
 
-    CALL fparser_PrintByteCode(rparser,iComp)
+    !CALL fparser_PrintByteCode(rparser,iComp)
 
   END SUBROUTINE fparser_parseFunction
 
