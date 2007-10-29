a50ca0775b1feb7f0962a4714196e0cb5cb00a46
Michael Koester <michael.koester@math.tu-dortmund.de>
2007-10-29 10:21:06 +0000

    New features        : - Added heatcond_method2 to makefile. Problems solved     : - Callback routines now dimensionless. Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@2302 4b12f95d-9313-0410-afc9-bd26427041f1

--- applications/heatconduction/src/heatcond_callback.f90
+++ applications/heatconduction/src/heatcond_callback.f90
@@ -93,7 +93,8 @@ CONTAINS
     ! This is an array of all points on all the elements where coefficients
     ! are needed.
     ! Remark: This usually coincides with rdomainSubset%p_DcubPtsReal.
-    REAL(DP), DIMENSION(NDIM2D,npointsPerElement,nelements), INTENT(IN)  :: Dpoints
+    ! DIMENSION(dimension,npointsPerElement,nelements)
+    REAL(DP), DIMENSION(:,:,:), INTENT(IN)  :: Dpoints
     
     ! An array accepting the DOF's on all elements trial in the trial space.
     ! DIMENSION(#local DOF's in trial space,nelements)
@@ -177,7 +178,8 @@ CONTAINS
     ! This is an array of all points on all the elements where coefficients
     ! are needed.
     ! Remark: This usually coincides with rdomainSubset%p_DcubPtsReal.
-    REAL(DP), DIMENSION(NDIM2D,npointsPerElement,nelements), INTENT(IN)  :: Dpoints
+    ! DIMENSION(dimension,npointsPerElement,nelements)
+    REAL(DP), DIMENSION(:,:,:), INTENT(IN)  :: Dpoints
 
     ! An array accepting the DOF's on all elements trial in the trial space.
     ! DIMENSION(#local DOF's in test space,nelements)
