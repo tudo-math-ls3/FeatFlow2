0bbff201405b8b4140b466742c82410d55ddace5
Michael Koester <michael.koester@math.tu-dortmund.de>
2007-11-20 12:05:21 +0000

    New features        : Problems solved     : Bugzilla-Bug-ID     : Additional comments : - Changed name of the equation from "Laplace problem" to "Poisson problem" which is mathematically more correct.
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@2402 4b12f95d-9313-0410-afc9-bd26427041f1

--- applications/poisson/src/poisson.f90
+++ applications/poisson/src/poisson.f90
@@ -33,7 +33,7 @@
 !# Multigrid solver with with ILU(0) smoother and UMFPACK-coarse grid solver.
 !#
 !# The 6th example (module poisson_method6) demonstrates like poisson_method1
-!# a simple way of solving the Laplace-equation, but this time introduces
+!# a simple way of solving the Poisson-equation, but this time introduces
 !# an additional fictitious boundary object inside of the domain.
 !#
 !# The 7th example is based again on example 1 and shows how to transform
@@ -73,61 +73,61 @@ PROGRAM poisson
 
   ! Call the problem to solve. Poisson 0:
   CALL output_lbrk ()
-  CALL output_line ('Calculating Laplace-Problem with method 0')
+  CALL output_line ('Calculating Poisson-Problem with method 0')
   CALL output_line ('-----------------------------------------')
   CALL poisson0
 
   ! Call the problem to solve. Poisson 0-mg:
   CALL output_lbrk ()
-  CALL output_line ('Calculating Laplace-Problem with method 0-mg')
+  CALL output_line ('Calculating Poisson-Problem with method 0-mg')
   CALL output_line ('--------------------------------------------')
   CALL poisson0_mg
 
   ! Call the problem to solve. Poisson 1:
   CALL output_lbrk ()
-  CALL output_line ('Calculating Laplace-Problem with method 1')
+  CALL output_line ('Calculating Poisson-Problem with method 1')
   CALL output_line ('-----------------------------------------')
   CALL poisson1
   
   ! Call the problem to solve. Poisson 2:
   CALL output_lbrk ()
-  CALL output_line ('Calculating Laplace-Problem with method 2')
+  CALL output_line ('Calculating Poisson-Problem with method 2')
   CALL output_line ('-----------------------------------------')
   CALL poisson2
   
   ! Call the problem to solve. Poisson 3:
   CALL output_lbrk ()
-  CALL output_line ('Calculating Laplace-Problem with method 3')
+  CALL output_line ('Calculating Poisson-Problem with method 3')
   CALL output_line ('-----------------------------------------')
   CALL poisson3
   
   ! Call the problem to solve. Poisson 4:
   CALL output_lbrk ()
-  CALL output_line ('Calculating Laplace-Problem with method 4')
+  CALL output_line ('Calculating Poisson-Problem with method 4')
   CALL output_line ('-----------------------------------------')
   CALL poisson4
   
   ! Call the problem to solve. Poisson 5:
   CALL output_lbrk ()
-  CALL output_line ('Calculating Laplace-Problem with method 5')
+  CALL output_line ('Calculating Poisson-Problem with method 5')
   CALL output_line ('-----------------------------------------')
   CALL poisson5
 
   ! Call the problem to solve. Poisson 6:
   CALL output_lbrk ()
-  CALL output_line ('Calculating Laplace-Problem with method 6')
+  CALL output_line ('Calculating Poisson-Problem with method 6')
   CALL output_line ('-----------------------------------------')
   CALL poisson6
 
   ! Call the problem to solve. Poisson 7:
   CALL output_lbrk ()
-  CALL output_line ('Calculating Laplace-Problem with method 7')
+  CALL output_line ('Calculating Poisson-Problem with method 7')
   CALL output_line ('-----------------------------------------')
   CALL poisson7
 
   ! Call the problem to solve. Poisson 8:
   CALL output_lbrk ()
-  CALL output_line ('Calculating Laplace-Problem with method 8')
+  CALL output_line ('Calculating Poisson-Problem with method 8')
   CALL output_line ('-----------------------------------------')
   CALL poisson8
 
