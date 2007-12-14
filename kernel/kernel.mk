8741c22e446029956c09474ce1c4816a8f4573e5
Matthias Moeller <matthias.moeller@math.tu-dortmund.de>
2007-12-14 15:47:40 +0000

    Added group finite element formulation for systems of conservation laws.
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@2534 4b12f95d-9313-0410-afc9-bd26427041f1

--- kernel/kernel.mk
+++ kernel/kernel.mk
@@ -33,7 +33,7 @@ KERNELSRC:=fsystem.f90 basicgeometry.f90 geometryaux.f90 afcutil.f90 \
 	graph.f90 octree.f90 triasearch.f90 statistics.f90 \
 	meshmodification.f90 mapleio.f90 jumpstabilisation.f90 \
         boundaryintegral.f90 afcstabilisation.f90 groupfemscalar.f90 \
-	hadaptaux.f90 hadaptaux2d.f90 hadaptaux3d.f90
+	hadaptaux.f90 hadaptaux2d.f90 hadaptaux3d.f90 groupfemsystem.f90
 
 # Include HDF5 subsystem if required
 ifeq ($(HDF5),YES)
