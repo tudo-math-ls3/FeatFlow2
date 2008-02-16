e9980c32dcaf5bb67efcf86e981c76a36e4a2e20
Matthias Moeller <matthias.moeller@math.tu-dortmund.de>
2008-02-16 19:32:25 +0000

    Fixed error in declaration of nonconforming linear element (EL_P1T).
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@2761 4b12f95d-9313-0410-afc9-bd26427041f1

--- kernel/ElementCubature/element.f90
+++ kernel/ElementCubature/element.f90
@@ -383,7 +383,7 @@ MODULE element
 !<constantblock description="Special 2D element variants.">
   
   ! ID of rotated linear nonconforming triangular FE, P1~, edge-midpoint based
-  INTEGER(I32), PARAMETER :: EL_E020 = EL_P1T
+  INTEGER(I32), PARAMETER :: EL_E020 = EL_P1T + 2**16
 
   ! ID of rotated bilinear conforming quadrilateral FE, Q1~, integral
   ! mean value based
