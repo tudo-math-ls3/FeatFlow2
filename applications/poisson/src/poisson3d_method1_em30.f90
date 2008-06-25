54ff05d677894689f8a873e1bf81a9c7716ca20a
Raphael Muenster <raphael.muenster@math.tu-dortmund.de>
2008-06-25 14:11:10 +0000

    New features        : adapted source code to the new mshreg_createFromNodalProp function Problems solved     : Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@3287 4b12f95d-9313-0410-afc9-bd26427041f1

--- applications/poisson/src/poisson3d_method1_em30.f90
+++ applications/poisson/src/poisson3d_method1_em30.f90
@@ -225,9 +225,8 @@ CONTAINS
     
     ! Create a mesh region describing the mesh's boundary based on the
     ! nodal-property-array of the current triangulation.
-    ! As we use the EM30 element here, which does only have DOFs in the faces,
-    ! we also only need to calculate the face index array of the mesh region.
-    CALL mshreg_createFromNodalProp(rmeshRegion, rtriangulation, MSHREG_IDX_FACE)
+    CALL mshreg_createFromNodalProp(rmeshRegion, rtriangulation, &
+                                      MSHREG_IDX_ALL)
     
     ! Describe Dirichlet BCs on that mesh region
     CALL bcasm_newDirichletBConMR(rdiscretisation, 1, rdiscreteBC, rmeshRegion,&
