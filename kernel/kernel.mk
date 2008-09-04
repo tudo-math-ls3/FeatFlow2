60dd57d8fec4564dd3b32bfd2fbe4b87da9c4842
Peter Zajac <peter.zajac@math.tu-dortmund.de>
2008-09-04 04:26:06 +0000

    New features        : Problems solved     : forgot to add vanka_aux.f90 to the kernel makefile and VS project file... Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@3575 4b12f95d-9313-0410-afc9-bd26427041f1

--- kernel/kernel.mk
+++ kernel/kernel.mk
@@ -35,7 +35,8 @@ KERNELSRC:=fsystem.f90 basicgeometry.f90 geometryaux.f90 afcutil.f90 \
 	boundaryintegral.f90 afcstabilisation.f90 groupfemscalar.f90 \
 	hadaptaux.f90 hadaptaux2d.f90 hadaptaux3d.f90 groupfemsystem.f90 \
 	meshregion.f90 pprocsolution.f90 elementpreprocessing.f90 iluk.f90 \
-	gmvwritef.c multileveloperators.f90 adjacency.f90 meshadjacency.f90
+	gmvwritef.c multileveloperators.f90 adjacency.f90 meshadjacency.f90 \
+	vanka_aux.f90
 
 # Include HDF5 subsystem if required
 ifeq ($(HDF5),YES)
