90a0aec26cbec51be79fe9edcd9e37a2cbbaef19
Matthias Moeller <matthias.moeller@math.tu-dortmund.de>
2008-10-14 18:22:14 +0000

    Changed keywords from uppercase to lowercase.
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@3747 4b12f95d-9313-0410-afc9-bd26427041f1

--- kernel/Preprocessing/pprocsolution.f90
+++ kernel/Preprocessing/pprocsolution.f90
@@ -18,49 +18,49 @@
 !#     -> Initialises a 2D double array from a Portable Graymap image
 !# </purpose>
 !#########################################################################
-MODULE pprocsolution
+module pprocsolution
 
-  USE fsystem
-  USE storage
-  USE io
-  USE linearsystemscalar
-  USE linearsystemblock
+  use fsystem
+  use storage
+  use io
+  use linearsystemscalar
+  use linearsystemblock
   
-  IMPLICIT NONE
+  implicit none
 
 !<types>
 !<typeblock>
 
   ! A portable Graymap
 
-  TYPE t_pgm
+  type t_pgm
 
     ! Identifier of the PGM
-    CHARACTER(LEN=2) :: id = ''
+    character(LEN=2) :: id = ''
 
     ! Width of the image
-    INTEGER :: width       = 0
+    integer :: width       = 0
 
     ! Height of the image
-    INTEGER :: height      = 0
+    integer :: height      = 0
 
     ! Maximum gray-value
-    INTEGER :: maxgray     = 0
+    integer :: maxgray     = 0
 
     ! Handle to the image data
-    INTEGER :: h_Idata     = ST_NOHANDLE
-  END TYPE t_pgm
+    integer :: h_Idata     = ST_NOHANDLE
+  end type t_pgm
 
 !</typeblock>
 !</types>
   
-CONTAINS
+contains
   
   ! ***************************************************************************
 
 !<subroutine>
 
-  SUBROUTINE ppsol_readPGM(ifile,sfile,rpgm)
+  subroutine ppsol_readPGM(ifile,sfile,rpgm)
 
 !<description>
     ! This subroutine reads a portable graymap image from file.
@@ -71,72 +71,72 @@ CONTAINS
     !  = 0: Get temporary channel for file 'sfile'
     ! <> 0: Write to channel ifile. Don't close the channel afterwards.
     !       'sfile' is ignored.
-    INTEGER(I32), INTENT(IN)     :: ifile
+    integer(I32), intent(IN)     :: ifile
     
     ! name of the file where to write to. Only relevant for ifile=0!
-    CHARACTER(len=*), INTENT(IN) :: sfile
+    character(len=*), intent(IN) :: sfile
 !</input>
 
 !<output>
     ! portable graymap
-    TYPE(t_pgm), INTENT(OUT)     :: rpgm
+    type(t_pgm), intent(OUT)     :: rpgm
 !</output>
 !</subroutine>
     
     ! local variables
-    CHARACTER(LEN=80) :: cbuffer
-    INTEGER,      DIMENSION(:,:), POINTER :: p_Idata
-    INTEGER(I32), DIMENSION(2) :: Isize
-    INTEGER :: cf,ix,iy
+    character(LEN=80) :: cbuffer
+    integer,      dimension(:,:), pointer :: p_Idata
+    integer(I32), dimension(2) :: Isize
+    integer :: cf,ix,iy
     
-    IF (ifile .EQ. 0) THEN
-      CALL io_openFileForReading(sfile, cf, .TRUE.)
-      IF (cf .EQ. -1) THEN
-        PRINT *, 'ppsol_readPGM: Could not open file '//trim(sfile)
-        CALL sys_halt()
-      END IF
-    ELSE
+    if (ifile .eq. 0) then
+      call io_openFileForReading(sfile, cf, .true.)
+      if (cf .eq. -1) then
+        print *, 'ppsol_readPGM: Could not open file '//trim(sfile)
+        call sys_halt()
+      end if
+    else
       cf = ifile
-    END IF
+    end if
 
     ! Read image ID
-    READ (cf,*) rpgm%id
+    read (cf,*) rpgm%id
 
 
-    SELECT CASE(rpgm%id)
-    CASE ('P2','p2')
+    select case(rpgm%id)
+    case ('P2','p2')
       ! Read the header
-      CALL getNextEntryASCII(cbuffer); READ(cbuffer,*) rpgm%width
-      CALL getNextEntryASCII(cbuffer); READ(cbuffer,*) rpgm%height
-      CALL getNextEntryASCII(cbuffer); READ(cbuffer,*) rpgm%maxgray
+      call getNextEntryASCII(cbuffer); read(cbuffer,*) rpgm%width
+      call getNextEntryASCII(cbuffer); read(cbuffer,*) rpgm%height
+      call getNextEntryASCII(cbuffer); read(cbuffer,*) rpgm%maxgray
     
       ! Allocate memory for image data
       Isize=(/rpgm%width, rpgm%height/)
-      CALL storage_new('ppsol_readPGM', 'h_Idata',&
+      call storage_new('ppsol_readPGM', 'h_Idata',&
           Isize, ST_INT, rpgm%h_Idata, ST_NEWBLOCK_NOINIT)
-      CALL storage_getbase_int2D(rpgm%h_Idata, p_Idata)
+      call storage_getbase_int2D(rpgm%h_Idata, p_Idata)
       
-      DO iy = 1, rpgm%height
-        DO ix = 1, rpgm%width
-          CALL getNextEntryASCII(cbuffer); READ(cbuffer,*) p_Idata(ix,iy)
-          IF (p_Idata(ix,iy) .GT. rpgm%maxgray) THEN
-            CALL output_line('Image data exceeds range!',&
+      do iy = 1, rpgm%height
+        do ix = 1, rpgm%width
+          call getNextEntryASCII(cbuffer); read(cbuffer,*) p_Idata(ix,iy)
+          if (p_Idata(ix,iy) .gt. rpgm%maxgray) then
+            call output_line('Image data exceeds range!',&
                 OU_CLASS_ERROR,OU_MODE_STD,'ppsol_readPGM')
-            CALL sys_halt()
-          END IF
-        END DO
-      END DO
+            call sys_halt()
+          end if
+        end do
+      end do
 
-    CASE DEFAULT
-      CALL output_line('Invalid PGM format!',&
+    case DEFAULT
+      call output_line('Invalid PGM format!',&
           OU_CLASS_ERROR,OU_MODE_STD,'ppsol_readPGM')
-      CALL sys_halt()
-    END SELECT
+      call sys_halt()
+    end select
       
     ! Close the file if necessary
-    IF (ifile .EQ. 0) CLOSE(cf)
+    if (ifile .eq. 0) close(cf)
 
-  CONTAINS
+  contains
 
     ! Here, the real working routines follow
     
@@ -146,56 +146,56 @@ CONTAINS
     ! Anything from character '#' till the end of line is ignored
     ! assuming Fortran's eor=eol ... seems to work
 
-    SUBROUTINE getNextEntryASCII(cbuffer)
+    subroutine getNextEntryASCII(cbuffer)
       
-      CHARACTER(LEN=*), INTENT(INOUT) :: cbuffer
+      character(LEN=*), intent(INOUT) :: cbuffer
 
-      CHARACTER(LEN=1) :: c
-      INTEGER          :: ipos
+      character(LEN=1) :: c
+      integer          :: ipos
 
       ipos = 1
       
       ! Read until somthing not a space, comma or comment
-      DO
-10      READ(cf, FMT='(a)', ADVANCE='NO', END=99, EOR=10) c
-        IF ((c .NE. ' ') .AND.&
-            (c .NE. ',') .AND.&
-            (c .NE.'\t') .AND.&
-            (c .NE. '#')) EXIT
-        IF (c .EQ. '#') THEN
+      do
+10      read(cf, FMT='(a)', ADVANCE='NO', end=99, EOR=10) c
+        if ((c .ne. ' ') .and.&
+            (c .ne. ',') .and.&
+            (c .ne.'\t') .and.&
+            (c .ne. '#')) exit
+        if (c .eq. '#') then
           ! Skip the comment till the end of line
-          DO
-            READ(cf, FMT='(A)', ADVANCE='NO', END=99, EOR=10) c
-          END DO
-        END IF
-      END DO
+          do
+            read(cf, FMT='(A)', ADVANCE='NO', end=99, EOR=10) c
+          end do
+        end if
+      end do
       
       ! We have some significant character, 
       ! read until next space, comma or comment
-      DO
+      do
         cbuffer(ipos:ipos) = c
         ipos = ipos+1
-        READ(cf, FMT='(a)', ADVANCE='NO', END=99, EOR=99) c
-        IF ((c .EQ. ' ') .OR.&
-            (c .EQ. ',') .OR.&
-            (c .EQ.'\t')) EXIT
-        IF (c .EQ. '#') THEN
+        read(cf, FMT='(a)', ADVANCE='NO', end=99, EOR=99) c
+        if ((c .eq. ' ') .or.&
+            (c .eq. ',') .or.&
+            (c .eq.'\t')) exit
+        if (c .eq. '#') then
           ! Skip the comment till the end of line
-          DO
-            READ(cf, FMT='(A)', ADVANCE='NO', END=99, EOR=99) c
-          END DO
-        END IF
-      END DO
+          do
+            read(cf, FMT='(A)', ADVANCE='NO', end=99, EOR=99) c
+          end do
+        end if
+      end do
       ! End the read characters by one space
 99    cbuffer(ipos:ipos) = ' '
-    END SUBROUTINE getNextEntryASCII
-  END SUBROUTINE ppsol_readPGM
+    end subroutine getNextEntryASCII
+  end subroutine ppsol_readPGM
 
   ! ***************************************************************************
 
 !<subroutine>
 
-  SUBROUTINE ppsol_releasePGM(rpgm)
+  subroutine ppsol_releasePGM(rpgm)
 
 !<description>
     ! This subroutine releases a portable graymap image.
@@ -203,7 +203,7 @@ CONTAINS
 
 !<inputoutput>
     ! portable graymap image
-    TYPE(t_pgm), INTENT(INOUT) :: rpgm
+    type(t_pgm), intent(INOUT) :: rpgm
 !</inputoutput>
 !</subroutine>
 
@@ -212,14 +212,14 @@ CONTAINS
     rpgm%height  = 0
     rpgm%maxgray = 0
 
-    CALL storage_free(rpgm%h_Idata)
-  END SUBROUTINE ppsol_releasePGM
+    call storage_free(rpgm%h_Idata)
+  end subroutine ppsol_releasePGM
 
   ! ***************************************************************************
 
 !<subroutine>
 
-  SUBROUTINE ppsol_initArrayPGM_Dble(rpgm, DvertexCoords, Ddata)
+  subroutine ppsol_initArrayPGM_Dble(rpgm, DvertexCoords, Ddata)
 
 !<description>
     ! Initialises a 2D double array by a Portable Graymap image
@@ -227,61 +227,61 @@ CONTAINS
 
 !<input>
     ! portable graymap image
-    TYPE(t_pgm), INTENT(IN)              :: rpgm
+    type(t_pgm), intent(IN)              :: rpgm
 
     ! vertex coordinates of 2D array
-    REAL(DP), DIMENSION(:,:), INTENT(IN) :: DvertexCoords
+    real(DP), dimension(:,:), intent(IN) :: DvertexCoords
 !</input>
 
 !<output>
     ! double data array
-    REAL(DP), DIMENSION(:), INTENT(OUT)  :: Ddata
+    real(DP), dimension(:), intent(OUT)  :: Ddata
 !</output>
 !</subroutine>
 
     ! local variables
-    INTEGER, DIMENSION(:,:), POINTER :: p_Idata
-    REAL(DP)     :: x,y,xmin,ymin,xmax,ymax
-    INTEGER(I32) :: ivt,nvt,ix,iy
+    integer, dimension(:,:), pointer :: p_Idata
+    real(DP)     :: x,y,xmin,ymin,xmax,ymax
+    integer(I32) :: ivt,nvt,ix,iy
 
     ! Set pointer for image data
-    CALL storage_getbase_int2D(rpgm%h_Idata, p_Idata)
+    call storage_getbase_int2D(rpgm%h_Idata, p_Idata)
 
     ! Determine minimum/maximum values of array
-    xmin = HUGE(DP); xmax = -HUGE(DP)
-    ymin = HUGE(DP); ymax = -HUGE(DP)
+    xmin = huge(DP); xmax = -huge(DP)
+    ymin = huge(DP); ymax = -huge(DP)
 
-    nvt = SIZE(Ddata)
-    DO ivt = 1, nvt
-      xmin = MIN(xmin, DvertexCoords(1,ivt))
-      xmax = MAX(xmax, DvertexCoords(1,ivt))
-      ymin = MIN(ymin, DvertexCoords(2,ivt))
-      ymax = MAX(ymax, DvertexCoords(2,ivt))
-    END DO
+    nvt = size(Ddata)
+    do ivt = 1, nvt
+      xmin = min(xmin, DvertexCoords(1,ivt))
+      xmax = max(xmax, DvertexCoords(1,ivt))
+      ymin = min(ymin, DvertexCoords(2,ivt))
+      ymax = max(ymax, DvertexCoords(2,ivt))
+    end do
 
     ! Clear array
-    CALL lalg_clearVectorDble(Ddata)
+    call lalg_clearVectorDble(Ddata)
 
     ! Fill array with scaled image data
-    DO ivt = 1, nvt
+    do ivt = 1, nvt
       x = DvertexCoords(1,ivt)
       y = DvertexCoords(2,ivt)
       
       ix = 1+(rpgm%width-1)*(x-xmin)/(xmax-xmin)
-      IF (ix .LT. 1 .OR. ix .GT. rpgm%width) CYCLE
+      if (ix .lt. 1 .or. ix .gt. rpgm%width) cycle
 
       iy = rpgm%height-(rpgm%height-1)*(y-ymin)/(ymax-ymin)
-      IF (iy .LT. 1 .OR. iy .GT. rpgm%height) CYCLE
+      if (iy .lt. 1 .or. iy .gt. rpgm%height) cycle
 
-      Ddata(ivt) = REAL(p_Idata(ix,iy),DP)/REAL(rpgm%maxgray,DP)
-    END DO
-  END SUBROUTINE ppsol_initArrayPGM_Dble
+      Ddata(ivt) = real(p_Idata(ix,iy),DP)/real(rpgm%maxgray,DP)
+    end do
+  end subroutine ppsol_initArrayPGM_Dble
 
   ! ***************************************************************************
 
 !<subroutine>
 
-  SUBROUTINE ppsol_initArrayPGM_Sngl(rpgm, DvertexCoords, Fdata)
+  subroutine ppsol_initArrayPGM_Sngl(rpgm, DvertexCoords, Fdata)
 
 !<description>
     ! Initialises a 2D single array by a Portable Graymap image
@@ -289,53 +289,53 @@ CONTAINS
 
 !<input>
     ! portable graymap image
-    TYPE(t_pgm), INTENT(IN)              :: rpgm
+    type(t_pgm), intent(IN)              :: rpgm
 
     ! vertex coordinates of 2D array
-    REAL(DP), DIMENSION(:,:), INTENT(IN) :: DvertexCoords
+    real(DP), dimension(:,:), intent(IN) :: DvertexCoords
 !</input>
 
 !<output>
     ! single data array
-    REAL(SP), DIMENSION(:), INTENT(OUT)  :: Fdata
+    real(SP), dimension(:), intent(OUT)  :: Fdata
 !</output>
 !</subroutine>
 
     ! local variables
-    INTEGER, DIMENSION(:,:), POINTER :: p_Idata
-    REAL(DP)     :: x,y,xmin,ymin,xmax,ymax
-    INTEGER(I32) :: ivt,nvt,ix,iy
+    integer, dimension(:,:), pointer :: p_Idata
+    real(DP)     :: x,y,xmin,ymin,xmax,ymax
+    integer(I32) :: ivt,nvt,ix,iy
 
     ! Set pointer for image data
-    CALL storage_getbase_int2D(rpgm%h_Idata, p_Idata)
+    call storage_getbase_int2D(rpgm%h_Idata, p_Idata)
 
     ! Determine minimum/maximum values of array
-    xmin = HUGE(DP); xmax = -HUGE(DP)
-    ymin = HUGE(DP); ymax = -HUGE(DP)
+    xmin = huge(DP); xmax = -huge(DP)
+    ymin = huge(DP); ymax = -huge(DP)
 
-    nvt = SIZE(Fdata)
-    DO ivt = 1, nvt
-      xmin = MIN(xmin, DvertexCoords(1,ivt))
-      xmax = MAX(xmax, DvertexCoords(1,ivt))
-      ymin = MIN(ymin, DvertexCoords(2,ivt))
-      ymax = MAX(ymax, DvertexCoords(2,ivt))
-    END DO
+    nvt = size(Fdata)
+    do ivt = 1, nvt
+      xmin = min(xmin, DvertexCoords(1,ivt))
+      xmax = max(xmax, DvertexCoords(1,ivt))
+      ymin = min(ymin, DvertexCoords(2,ivt))
+      ymax = max(ymax, DvertexCoords(2,ivt))
+    end do
 
     ! Clear array
-    CALL lalg_clearVectorSngl(Fdata)
+    call lalg_clearVectorSngl(Fdata)
 
     ! Fill array with scaled image data
-    DO ivt = 1, nvt
+    do ivt = 1, nvt
       x = DvertexCoords(1,ivt)
       y = DvertexCoords(2,ivt)
       
       ix = 1+(rpgm%width-1)*(x-xmin)/(xmax-xmin)
-      IF (ix .LT. 1 .OR. ix .GT. rpgm%width) CYCLE
+      if (ix .lt. 1 .or. ix .gt. rpgm%width) cycle
 
       iy = rpgm%height-(rpgm%height-1)*(y-ymin)/(ymax-ymin)
-      IF (iy .LT. 1 .OR. iy .GT. rpgm%height) CYCLE
+      if (iy .lt. 1 .or. iy .gt. rpgm%height) cycle
 
-      Fdata(ivt) = REAL(p_Idata(ix,iy),SP)/REAL(rpgm%maxgray,SP)
-    END DO
-  END SUBROUTINE ppsol_initArrayPGM_Sngl
-END MODULE pprocsolution
+      Fdata(ivt) = real(p_Idata(ix,iy),SP)/real(rpgm%maxgray,SP)
+    end do
+  end subroutine ppsol_initArrayPGM_Sngl
+end module pprocsolution
