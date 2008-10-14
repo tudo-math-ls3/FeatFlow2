7bee17f6d20f1ca78eb5703fa3594daf4a665c3c
Matthias Moeller <matthias.moeller@math.tu-dortmund.de>
2008-10-14 18:47:48 +0000

    Changed keywords from uppercase to lowercase.
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@3759 4b12f95d-9313-0410-afc9-bd26427041f1

--- codefragments/bdryintegralerror/bdryintegralerror.f90
+++ codefragments/bdryintegralerror/bdryintegralerror.f90
@@ -19,20 +19,20 @@
 !# </purpose>
 !##############################################################################
 
-MODULE bdryintegralerror
+module bdryintegralerror
 
-  USE fsystem
-  USE boundaryintegral
+  use fsystem
+  use boundaryintegral
 
-  PRIVATE :: ffunctionBoundaryError
+  private :: ffunctionBoundaryError
 
-CONTAINS
+contains
 
   !****************************************************************************
 
 !<subroutine>
 
-  SUBROUTINE bdrierr_referenceFunction (dx,dy,dvalue,dderivX,dderivY)
+  subroutine bdrierr_referenceFunction (dx,dy,dvalue,dderivX,dderivY)
 
 !<description>
   ! USER DEFINED CALLBACK ROUTINE.
@@ -42,31 +42,31 @@ CONTAINS
 
 !<input>
   ! Coordinates of the point.
-  REAL(DP), INTENT(IN) :: dx,dy
+  real(DP), intent(IN) :: dx,dy
 !</input>
 
 !<output>
   ! Value of the reference function.
-  REAL(DP), INTENT(OUT) :: dvalue
+  real(DP), intent(OUT) :: dvalue
 
   ! X-derivative
-  REAL(DP), INTENT(OUT) :: dderivX
+  real(DP), intent(OUT) :: dderivX
 
   ! Y-derivative
-  REAL(DP), INTENT(OUT) :: dderivY
+  real(DP), intent(OUT) :: dderivY
 !</output>
 
     dvalue = 0.0_DP
     dderivX = 0.0_DP
     dderivY = 0.0_DP
 
-  END SUBROUTINE
+  end subroutine
 
   !****************************************************************************
 
 !<subroutine>
 
-  SUBROUTINE bdrierr_calcError (rvectorScalar,ccubType,dvalue,rboundaryRegion)
+  subroutine bdrierr_calcError (rvectorScalar,ccubType,dvalue,rboundaryRegion)
 
 !<description>
   ! Calculates the boundary integral error to the reference function $u$.
@@ -80,58 +80,58 @@ CONTAINS
 
 !<input>
   ! A scalar FE function to compute a boundary integral of.
-  TYPE(t_vectorScalar), INTENT(IN) :: rvectorScalar
+  type(t_vectorScalar), intent(IN) :: rvectorScalar
 
   ! A line cubature formula CUB_xxxx_1D to be used for line integration.
-  INTEGER, INTENT(IN) :: ccubType
+  integer, intent(IN) :: ccubType
   
   ! OPTIONAL: A t_boundaryRegion specifying the boundary region where
   ! to calculate. If not specified, the computation is done over
   ! the whole boundary.
-  TYPE(t_boundaryRegion), INTENT(IN), OPTIONAL :: rboundaryRegion
+  type(t_boundaryRegion), intent(IN), optional :: rboundaryRegion
 !</input>
 
 !<output>
   ! The calculated value of the integral.
-  REAL(DP), INTENT(OUT) :: dvalue
+  real(DP), intent(OUT) :: dvalue
 !</output>
 
     ! local variables
-    TYPE(t_spatialDiscretisation), POINTER :: p_rdiscr
-    TYPE(t_collection) :: rcollection
+    type(t_spatialDiscretisation), pointer :: p_rdiscr
+    type(t_collection) :: rcollection
     
     ! Get the underlying discretisation
     p_rdiscr => rvectorScalar%p_rspatialDiscretisation
 
     ! Create a collection that allows us to pass the vector to
     ! the callback routine
-    CALL collct_init(rcollection)
+    call collct_init(rcollection)
     
     ! Save the vector to the collection, so we can restore it in
     ! the callback routine
-    CALL collct_setvalue_vecsca (rcollection, 'vector', rvectorScalar, .TRUE.) 
+    call collct_setvalue_vecsca (rcollection, 'vector', rvectorScalar, .true.) 
 
-    CALL bdint_integral2D (p_rdiscr%p_rboundary,p_rdiscr%p_rtriangulation,&
+    call bdint_integral2D (p_rdiscr%p_rboundary,p_rdiscr%p_rtriangulation,&
       ccubType,ffunctionBoundaryError,dvalue,rboundaryRegion,rcollection)
       
     ! Release the collection
-    CALL collct_deletevalue (rcollection,'vector')
-    CALL collct_done(rcollection)
+    call collct_deletevalue (rcollection,'vector')
+    call collct_done(rcollection)
 
-  END SUBROUTINE
+  end subroutine
 
   !****************************************************************************
 
   !<subroutine>
 
-  SUBROUTINE ffunctionBoundaryError (DpointsRef, Dpoints, ibct, DpointPar, Ielements, &
+  subroutine ffunctionBoundaryError (DpointsRef, Dpoints, ibct, DpointPar, Ielements, &
         p_rcollection, Dvalues)
     
-    USE basicgeometry
-    USE triangulation
-    USE collection
-    USE scalarpde
-    USE domainintegration
+    use basicgeometry
+    use triangulation
+    use collection
+    use scalarpde
+    use domainintegration
     
   !<description>
     ! AUXILIARY FUNCTION.
@@ -155,34 +155,34 @@ CONTAINS
     ! information is needed. These coordinates correspond to the reference
     ! element.
     ! DIMENSION(NDIM2D,npointsPerElement,nelements)
-    REAL(DP), DIMENSION(:,:,:), INTENT(IN)  :: DpointsRef
+    real(DP), dimension(:,:,:), intent(IN)  :: DpointsRef
     
     ! This is an array of all points on all the elements where coefficients
     ! are needed. It specifies the coordinates of the points where
     ! information is needed. These coordinates are world coordinates,
     ! i.e. on the real element.
     ! DIMENSION(NDIM2D,npointsPerElement,nelements)
-    REAL(DP), DIMENSION(:,:,:), INTENT(IN)  :: Dpoints
+    real(DP), dimension(:,:,:), intent(IN)  :: Dpoints
     
     ! This is the number of the boundary component that contains the
     ! points in Dpoint. All points are on the same boundary component.
-    INTEGER, INTENT(IN) :: ibct
+    integer, intent(IN) :: ibct
     
     ! For every point under consideration, this specifies the parameter
     ! value of the point on the boundary component. The parameter value
     ! is calculated in LENGTH PARAMETRISATION!
     ! DIMENSION(npointsPerElement,nelements)
-    REAL(DP), DIMENSION(:,:), INTENT(IN) :: DpointPar
+    real(DP), dimension(:,:), intent(IN) :: DpointPar
     
     ! This is a list of elements (corresponding to Dpoints) where information
     ! is needed. To an element iel=Ielements(i), the array Dpoints(:,:,i)
     ! specifies the points where information is needed.
     ! DIMENSION(nelements)
-    INTEGER(PREC_ELEMENTIDX), DIMENSION(:), INTENT(IN) :: Ielements
+    integer(PREC_ELEMENTIDX), dimension(:), intent(IN) :: Ielements
 
     ! A pointer to a collection structure to provide additional 
     ! information to the coefficient routine. May point to NULL() if not defined.
-    TYPE(t_collection), POINTER                      :: p_rcollection
+    type(t_collection), pointer                      :: p_rcollection
   !</input>
   
   !<output>
@@ -190,20 +190,20 @@ CONTAINS
     ! in all the points specified in Dpoints, or the appropriate derivative
     ! of the function, respectively, according to cderivative.
     !   DIMENSION(npointsPerElement,nelements)
-    REAL(DP), DIMENSION(:,:), INTENT(OUT)                      :: Dvalues
+    real(DP), dimension(:,:), intent(OUT)                      :: Dvalues
   !</output>
     
   !</subroutine>
 
     ! local variables
-    TYPE(t_vectorScalar), POINTER :: p_rvector
-    TYPE(t_boundary), POINTER :: p_rboundary
-    REAL(DP), DIMENSION(:,:), ALLOCATABLE :: Duh
-    REAL(DP), DIMENSION(:,:,:), ALLOCATABLE :: Du
-    INTEGER(PREC_ELEMENTIDX) :: iel
-    INTEGER :: ipoint
-    REAL(DP) :: dminPar,dmaxPar
-    REAL(DP) :: dnx,dny,dt
+    type(t_vectorScalar), pointer :: p_rvector
+    type(t_boundary), pointer :: p_rboundary
+    real(DP), dimension(:,:), allocatable :: Duh
+    real(DP), dimension(:,:,:), allocatable :: Du
+    integer(PREC_ELEMENTIDX) :: iel
+    integer :: ipoint
+    real(DP) :: dminPar,dmaxPar
+    real(DP) :: dnx,dny,dt
     
     ! Get the vector with the FE function from the collection
     ! and to the boundary definition
@@ -214,39 +214,39 @@ CONTAINS
     ! DU(1,:,:) = function value,
     ! DU(2,:,:) = X-derivative,
     ! DU(3,:,:) = Y-derivative
-    ALLOCATE(Duh(UBOUND(Dvalues,1),UBOUND(Dvalues,2)))
-    ALLOCATE(Du(3,UBOUND(Dvalues,1),UBOUND(Dvalues,2)))
+    allocate(Duh(ubound(Dvalues,1),ubound(Dvalues,2)))
+    allocate(Du(3,ubound(Dvalues,1),ubound(Dvalues,2)))
     
-    DO iel=1,SIZE(Ielements)
+    do iel=1,size(Ielements)
       ! Evaluate the FE function in the given points.
-      CALL fevl_evaluate_mult (DER_FUNC, Duh(:,iel), p_rvector, &
+      call fevl_evaluate_mult (DER_FUNC, Duh(:,iel), p_rvector, &
           Ielements(iel), DpointsRef(:,:,iel), Dpoints(:,:,iel))
           
       ! Evaluate the analytical function in the given points
-      DO ipoint = 1,UBOUND(Dvalues,1)
-        CALL bdrierr_referenceFunction (&
+      do ipoint = 1,ubound(Dvalues,1)
+        call bdrierr_referenceFunction (&
             Dpoints(1,ipoint,iel),Dpoints(2,ipoint,iel),&
             Du(1,ipoint,iel),Du(2,ipoint,iel),Du(3,ipoint,iel))
-      END DO
-    END DO
+      end do
+    end do
 
     ! Get the minimum and maximum parameter value. The point with the minimal
     ! parameter value is the start point of the interval, the point with the
     ! maximum parameter value the endpoint.
     dminPar = DpointPar(1,1)
     dmaxPar = DpointPar(1,1)
-    DO iel=1,SIZE(Ielements)
-      DO ipoint = 1,UBOUND(Dpoints,1)
-        dminPar = MIN(DpointPar(ipoint,iel),dminPar)
-        dmaxPar = MAX(DpointPar(ipoint,iel),dmaxPar)
-      END DO
-    END DO
+    do iel=1,size(Ielements)
+      do ipoint = 1,ubound(Dpoints,1)
+        dminPar = min(DpointPar(ipoint,iel),dminPar)
+        dmaxPar = max(DpointPar(ipoint,iel),dmaxPar)
+      end do
+    end do
 
     ! Calculate the value of the term in the integral in that point.
-    DO iel=1,SIZE(Ielements)
+    do iel=1,size(Ielements)
     
       ! Evaluate the analytical function in the given points
-      DO ipoint = 1,UBOUND(Dvalues,1)
+      do ipoint = 1,ubound(Dvalues,1)
        
         ! Parameter value of the point -- in length parametrisation!
         dt = DpointPar(ipoint,iel)
@@ -260,32 +260,32 @@ CONTAINS
         ! the endpoints of a boundary edge. In such a case, the routine
         ! would normally compute the normal vector as a mean on the
         ! normal vectors of the edges adjacent to such a point!
-        IF (dt .EQ. dminPar) THEN
+        if (dt .eq. dminPar) then
           ! Start point
-          CALL boundary_getNormalVec(p_rboundary, ibct, &
+          call boundary_getNormalVec2D(p_rboundary, ibct, &
               dt, dnx, dny,BDR_NORMAL_RIGHT,BDR_PAR_LENGTH)
-        ELSE IF (dt .EQ. dmaxPar) THEN
+        else if (dt .eq. dmaxPar) then
           ! End point
-          CALL boundary_getNormalVec(p_rboundary, ibct, &
+          call boundary_getNormalVec2D(p_rboundary, ibct, &
               dt, dnx, dny,BDR_NORMAL_LEFT,BDR_PAR_LENGTH)
-        ELSE
+        else
           ! Inner point
-          CALL boundary_getNormalVec(p_rboundary, ibct, &
+          call boundary_getNormalVec2D(p_rboundary, ibct, &
               dt, dnx, dny,cparType=BDR_PAR_LENGTH)
-        END IF
+        end if
         
         ! Calculate the function value: (u-u_h) d_n(u)
         Dvalues(ipoint,iel) = &
             ( Du(1,ipoint,iel) - Duh(ipoint,iel)) * &
             ( Du(2,ipoint,iel)*dnx + Du(3,ipoint,iel)*dny )  
             
-      END DO
+      end do
       
-    END DO
+    end do
     
     ! Release memory
-    DEALLOCATE(Du,Duh)
+    deallocate(Du,Duh)
 
-  END SUBROUTINE
+  end subroutine
   
-END MODULE
+end module
