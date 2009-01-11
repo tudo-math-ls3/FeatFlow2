c3312e0b6cdb2874dae5f6866bd43e0f8b9e4795
Peter Zajac <peter.zajac@math.tu-dortmund.de>
2009-01-11 15:06:23 +0000

    New features        : Problems solved     : fixed a bug in fully-coupled Vanka Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@4033 4b12f95d-9313-0410-afc9-bd26427041f1

--- kernel/LinearSolver/vanka_navst2d.f90
+++ kernel/LinearSolver/vanka_navst2d.f90
@@ -2590,7 +2590,7 @@ contains
             j = p_KcolB(i)
             daux = p_DvecP(j)
             daux1 = daux1 + p_DB1(i)*daux
-            daux2 = daux2 + p_DB2(i)*daux2
+            daux2 = daux2 + p_DB2(i)*daux
             do l = 1, ndofP
               if(j .eq. IdofP(l)) then
                 Da(  k,p+l) = Dmult(1,3)*p_DB1(i)
