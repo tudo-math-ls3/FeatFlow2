83fbc95181548bb93a47f6b17a3abbb2b896e438
Raphael Muenster <raphael.muenster@math.tu-dortmund.de>
2009-02-26 17:48:46 +0000

    New features        : adjustments to the function interfaces Problems solved     : Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@4486 4b12f95d-9313-0410-afc9-bd26427041f1

--- area51/tridef/src/tridef2d_method0.f90
+++ area51/tridef/src/tridef2d_method0.f90
@@ -9,37 +9,37 @@
 !# </purpose>
 !##############################################################################
 
-MODULE tridef2d_method0
+module tridef2d_method0
 
-  USE fsystem
-  USE genoutput
-  USE storage
-  USE linearsolver
-  USE boundary
-  USE bilinearformevaluation
-  USE linearformevaluation
-  USE cubature
-  USE matrixfilters
-  USE vectorfilters
-  USE bcassembly
-  USE triangulation
-  USE spatialdiscretisation
-  USE ucd
-  USE pprocerror
-  USE genoutput
-  USE griddeform
+  use fsystem
+  use genoutput
+  use storage
+  use linearsolver
+  use boundary
+  use bilinearformevaluation
+  use linearformevaluation
+  use cubature
+  use matrixfilters
+  use vectorfilters
+  use bcassembly
+  use triangulation
+  use spatialdiscretisation
+  use ucd
+  use pprocerror
+  use genoutput
+  use griddeform
     
-  USE tridef2d_callback
+  use tridef2d_callback
   
-  IMPLICIT NONE
+  implicit none
 
-CONTAINS
+contains
 
   ! ***************************************************************************
 
 !<subroutine>
 
-  SUBROUTINE tridef2d_simple
+  subroutine tridef2d_simple
   
 !<description>
   ! In this program we are only concerned with a
@@ -62,85 +62,89 @@ CONTAINS
     ! We need a couple of variables for this problem. Let's see...
     !
     ! An object for saving the domain:
-    TYPE(t_boundary) :: rboundary
+    type(t_boundary) :: rboundary
     
-    TYPE(t_griddefWork) :: rgriddefWork
+    type(t_griddefWork) :: rgriddefWork
     
     ! An object for saving the triangulation on the domain
-    TYPE(t_triangulation) :: rtriangulation
+    type(t_triangulation) :: rtriangulation
 
     ! An object specifying the discretisation.
     ! This contains also information about trial/test functions,...
-    TYPE(t_blockDiscretisation) :: rdiscretisation
+    type(t_blockDiscretisation) :: rdiscretisation
     
     ! In this vector we will save the area of the deformed grid
-    TYPE(t_vectorScalar) :: rvectorAreaQ0 
+    type(t_vectorScalar) :: rvectorAreaQ0 
 
     ! NLMAX receives the level where we want to solve.
-    INTEGER :: NLMAX
+    integer :: NLMAX,NLMIN
     
     ! dummy
-    INTEGER :: idummy    
+    integer :: idummy,iloop    
     
     ! Output block for UCD output to GMV file
-    TYPE(t_ucdExport) :: rexport
+    type(t_ucdExport) :: rexport
     
     ! To these arrays we write the monitorfunction,
     ! elementArea, rhs, etc... in order to
     ! check these values in gmv
-    REAL(DP), DIMENSION(:), POINTER :: p_Ddata
-    REAL(DP), DIMENSION(:), POINTER :: p_Ddata1
-    REAL(DP), DIMENSION(:), POINTER :: p_Ddata2        
-    REAL(DP), DIMENSION(:), POINTER :: p_Ddata3 
-    REAL(DP), DIMENSION(:), POINTER :: p_Ddata4 
-    REAL(DP), DIMENSION(:), POINTER :: p_Ddata5 
-    REAL(DP), DIMENSION(:), POINTER :: p_Ddata6 
-    REAL(DP), DIMENSION(:), POINTER :: p_Ddata7 
-    TYPE(t_vectorBlock) :: rvectorAreaBlockQ0
-    TYPE(t_vectorBlock) :: rvectorAreaBlockQ1                 
+    real(dp), dimension(:), pointer :: p_Ddata
+    real(dp), dimension(:), pointer :: p_Ddata1
+    real(dp), dimension(:), pointer :: p_Ddata2        
+    real(dp), dimension(:), pointer :: p_Ddata3 
+    real(dp), dimension(:), pointer :: p_Ddata4 
+    real(dp), dimension(:), pointer :: p_Ddata5 
+    real(dp), dimension(:), pointer :: p_Ddata6 
+    real(dp), dimension(:), pointer :: p_Ddata7 
+    type(t_vectorBlock) :: rvectorAreaBlockQ0
+    type(t_vectorBlock) :: rvectorAreaBlockQ1                 
     
     ! grid deform structure
-    TYPE(t_griddefInfo) :: rgriddefInfo
+    type(t_griddefInfo) :: rgriddefInfo
 
     ! Ok, let's start. 
     !
     ! Here we set the level of the
     ! grid we want to deform
-    NLMAX = 5
+    NLMAX = 6
+    NLMIN = 6
     
     ! At first, read in the parametrisation of the boundary and save
     ! it to rboundary.
-    CALL boundary_read_prm(rboundary, './pre/bench2m55.prm')
+    call boundary_read_prm(rboundary, './pre/QUAD.prm')
         
     ! Now read in the basic triangulation.
-    CALL tria_readTriFile2D (rtriangulation, './pre/bench2m55.tri', rboundary)
+    call tria_readTriFile2D (rtriangulation, './pre/QUAD.tri', rboundary)
      
     ! Refine it.
-    CALL tria_quickRefine2LevelOrdering (NLMAX-1,rtriangulation,rboundary)
+    call tria_quickRefine2LevelOrdering (NLMAX-1,rtriangulation,rboundary)
     
     ! And create information about adjacencies and everything one needs from
     ! a triangulation.
-    CALL tria_initStandardMeshFromRaw (rtriangulation,rboundary)
+    call tria_initStandardMeshFromRaw (rtriangulation,rboundary)
     
     ! Now we can start to initialise the discretisation. At first, set up
     ! a block discretisation structure that specifies the blocks in the
     ! solution vector. In this simple problem, we only have one block.
-    CALL spdiscr_initBlockDiscr (rdiscretisation,1,&
+    call spdiscr_initBlockDiscr (rdiscretisation,1,&
                                    rtriangulation, rboundary)
     
     ! rdiscretisation%Rdiscretisations is a list of scalar discretisation
     ! structures for every component of the solution vector.
     ! Initialise the first element of the list to specify the element
     ! and cubature rule for this solution component:
-    CALL spdiscr_initDiscr_simple (rdiscretisation%RspatialDiscr(1), &
+    call spdiscr_initDiscr_simple (rdiscretisation%RspatialDiscr(1), &
                                    EL_E011,CUB_G2X2,rtriangulation, rboundary)
     
     ! Discretisation is set up, now go for the griddeformation
-    CALL griddef_deformationInit(rgriddefInfo,rtriangulation,NLMAX,rboundary,10)                 
+    call griddef_deformationInit(rgriddefInfo,rtriangulation,NLMIN,NLMAX,rboundary,10)                 
     
+    do iloop=NLMIN,NLMAX
+      call griddef_buildHGrid(rgriddefInfo,rtriangulation,iloop)
+    end do
     
     ! Deform
-    CALL griddef_performDeformation(rgriddefInfo, rgriddefWork,idummy,&
+    call griddef_performDeformation(rgriddefInfo, rgriddefWork,idummy,&
                                      rdiscretisation,&
                                     .TRUE., .FALSE., .FALSE., &
                                     .FALSE., NLMAX, 0, 0,rboundary,&
@@ -150,70 +154,72 @@ CONTAINS
     ! start the postprocessing. 
     ! Start UCD export to GMV file:
 
-    CALL ucd_startGMV (rexport,UCD_FLAG_STANDARD,rtriangulation,&
+    call ucd_startGMV (rexport,UCD_FLAG_STANDARD,rtriangulation,&
                        'gmv/u2d_0_simple1.gmv')
     
-    CALL lsyssc_getbase_double (rgriddefWork%rvecGradBlock%RvectorBlock(1),p_Ddata)
-    CALL ucd_addVariableVertexBased (rexport,'Ux',UCD_VAR_STANDARD, p_Ddata)
+    call lsyssc_getbase_double (rgriddefWork%rvecGradBlock%RvectorBlock(1),p_Ddata)
+    call ucd_addVariableVertexBased (rexport,'Ux',UCD_VAR_STANDARD, p_Ddata)
     
-    CALL lsyssc_getbase_double (rgriddefWork%rvecGradBlock%RvectorBlock(2),p_Ddata1)
-    CALL ucd_addVariableVertexBased (rexport,'Uy',UCD_VAR_STANDARD, p_Ddata1)
+    call lsyssc_getbase_double (rgriddefWork%rvecGradBlock%RvectorBlock(2),p_Ddata1)
+    call ucd_addVariableVertexBased (rexport,'Uy',UCD_VAR_STANDARD, p_Ddata1)
     
-    CALL lsyssc_getbase_double (rgriddefWork%rSolBlock%RvectorBlock(1),p_Ddata2)
-    CALL ucd_addVariableVertexBased (rexport,'Sol',UCD_VAR_STANDARD, p_Ddata2)
+    call lsyssc_getbase_double (rgriddefWork%rSolBlock%RvectorBlock(1),p_Ddata2)
+    call ucd_addVariableVertexBased (rexport,'Sol',UCD_VAR_STANDARD, p_Ddata2)
     
-    CALL lsyssc_getbase_double (rgriddefWork%rrhsBlock%RvectorBlock(1),p_Ddata3)
-    CALL ucd_addVariableVertexBased (rexport,'rhs',UCD_VAR_STANDARD, p_Ddata3) 
+    call lsyssc_getbase_double (rgriddefWork%rrhsBlock%RvectorBlock(1),p_Ddata3)
+    call ucd_addVariableVertexBased (rexport,'rhs',UCD_VAR_STANDARD, p_Ddata3) 
 
-    CALL lsyssc_getbase_double (rgriddefWork%rvectorAreaBlockQ1%RvectorBlock(1),p_Ddata4)
-    CALL ucd_addVariableVertexBased (rexport,'area',UCD_VAR_STANDARD, p_Ddata4) 
+    call lsyssc_getbase_double (rgriddefWork%rvectorAreaBlockQ1%RvectorBlock(1),p_Ddata4)
+    call ucd_addVariableVertexBased (rexport,'area',UCD_VAR_STANDARD, p_Ddata4) 
 
-    CALL lsyssc_getbase_double (rgriddefWork%rvectorMonFuncQ1%RvectorBlock(1),p_Ddata5)
-    CALL ucd_addVariableVertexBased (rexport,'Mon',UCD_VAR_STANDARD, p_Ddata5) 
+    call lsyssc_getbase_double (rgriddefWork%rvectorMonFuncQ1%RvectorBlock(1),p_Ddata5)
+    call ucd_addVariableVertexBased (rexport,'Mon',UCD_VAR_STANDARD, p_Ddata5) 
     
-    ! Write the file to disc, that's it.
-    CALL ucd_write (rexport)
-    CALL ucd_release (rexport)
+    ! write the file to disc, that's it.
+    call ucd_write (rexport)
+    call ucd_release (rexport)
 
-    CALL griddef_getAreaDeformed(rgriddefInfo,&
+    call griddef_getAreaDeformed(rgriddefInfo,&
              rvectorAreaBlockQ0,rvectorAreaBlockQ1,rvectorAreaQ0)
     
-    CALL ucd_startGMV (rexport,UCD_FLAG_STANDARD,rgriddefInfo%rDeftriangulation,&
+    call ucd_startGMV (rexport,UCD_FLAG_STANDARD,rgriddefInfo%p_rhLevels(NLMAX)%rtriangulation,&
                        'gmv/u2d_0_simple_grid1.gmv')
 
     ! Area in den Zellen und Area in den Knoten    
     
-    CALL lsysbl_getbase_double (rvectorAreaBlockQ0,p_Ddata6)
-    CALL ucd_addVariableElementBased (rexport,'AreaCells',UCD_VAR_STANDARD, p_Ddata6) 
+    call lsysbl_getbase_double (rvectorAreaBlockQ0,p_Ddata6)
+    call ucd_addVariableElementBased (rexport,'AreaCells',UCD_VAR_STANDARD, p_Ddata6) 
     
-    CALL lsysbl_getbase_double (rvectorAreaBlockQ1,p_Ddata7)
-    CALL ucd_addVariableVertexBased (rexport,'AreaNode',UCD_VAR_STANDARD, p_Ddata7) 
+    call lsysbl_getbase_double (rvectorAreaBlockQ1,p_Ddata7)
+    call ucd_addVariableVertexBased (rexport,'AreaNode',UCD_VAR_STANDARD, p_Ddata7) 
     
-    CALL ucd_write (rexport)
-    CALL ucd_release (rexport)
+    call ucd_write (rexport)
+    call ucd_release (rexport)
+
+    ! Release the triangulation. 
+    call tria_done (rgriddefInfo%p_rhLevels(NLMAX)%rtriangulation)
+    call tria_done (rtriangulation)
     
     ! Release Deformation structures
-    CALL griddef_DeformationDone(rgriddefInfo,rgriddefWork)
+    call griddef_DeformationDone(rgriddefInfo,rgriddefWork)
 
     
     ! Release the discretisation structure and all spatial discretisation
     ! structures in it.
-    CALL spdiscr_releaseBlockDiscr(rdiscretisation)
+    call spdiscr_releaseBlockDiscr(rdiscretisation)
     
-    ! Release the triangulation. 
-    CALL tria_done (rgriddefInfo%rDeftriangulation)    
-    CALL tria_done (rtriangulation)
     
-    CALL lsysbl_releaseVector(rvectorAreaBlockQ0)
-    CALL lsysbl_releaseVector(rvectorAreaBlockQ1) 
-    CALL lsyssc_releaseVector(rvectorAreaQ0) 
+    call lsysbl_releaseVector(rvectorAreaBlockQ0)
+    call lsysbl_releaseVector(rvectorAreaBlockQ1) 
+    call lsyssc_releaseVector(rvectorAreaQ0) 
     
     ! Finally release the domain, that's it.
-    CALL boundary_release (rboundary)
+    call boundary_release (rboundary)
     
-  END SUBROUTINE
+  end subroutine
+
+!******************************************************************************
   
-!******************************************************************************  
   
   !<subroutine>  
     subroutine tridef2d_monitorfct(DvertexCoords,Dentries)
