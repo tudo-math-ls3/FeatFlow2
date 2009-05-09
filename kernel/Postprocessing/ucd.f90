ba77a29300b297164e7d8936ac708cce92571c6c
Michael Koester <michael.koester@math.tu-dortmund.de>
2009-05-09 15:41:47 +0000

    New features        : Problems solved     : - FIXED: Strange data format string Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@5092 4b12f95d-9313-0410-afc9-bd26427041f1

--- kernel/Postprocessing/ucd.f90
+++ kernel/Postprocessing/ucd.f90
@@ -346,7 +346,7 @@ module ucd
     character(LEN=SYS_STRLEN) :: ssimTimeFormat = "(F20.5)"
     
     ! Format of the output of double-precision numbers. Fortran format string.
-    character(LEN=SYS_STRLEN) :: sdataFormat = "(E16.8E3)"
+    character(LEN=SYS_STRLEN) :: sdataFormat = "(E16.8)"
     
     ! An array containing the names of all the variables
     character(LEN=SYS_NAMELEN), dimension(:), pointer :: p_SvariableNames => null()
