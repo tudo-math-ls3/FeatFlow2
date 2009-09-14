98dba475cf4f95e8d1dd41942f7414fc09efe58b
Andriy Sokolov <andriy.sokolov@math.tu-dortmund.de>
2009-09-14 13:20:21 +0000

    New features        : Problems solved     : Bugzilla-Bug-ID     : Additional comments : Read the chemotaxis data parameters
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@6143 4b12f95d-9313-0410-afc9-bd26427041f1

--- area51/bio_app3d/src/chemotaxis_pattern_FCT.f90
+++ area51/bio_app3d/src/chemotaxis_pattern_FCT.f90
@@ -111,8 +111,126 @@ contains
 
 !</subroutine>
 
-    ! Definitions of variables.
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    !!!!! Definitions of variables. !!!!! 
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ! A parameter list structure accepting the parameters from the DAT file.
+    type(t_parlist) :: rparams
     !
+    !!!!! parameters' variables !!!!! 
+    ! maximum level of refinment
+    integer :: NLMAX    
+    !
+    ! Defining the output level
+    ! =0 means no gmv files generated
+    ! =1 means gmv files printed
+    ! =2 means just the gmv file is printed
+    ! =3 means just the ISTEP_GMV-th gmv file are printed
+    integer :: OUTPUT, ISTEP_GMV
+    !
+    ! Whether or not taking an const initial sol
+    ! =0 means const initial sol
+    ! =1 means exponential initial sol
+    ! Whether or not we set the IC by l_2 projection ( = 1 ) or by interpolation ( =0 )
+    integer :: INITSOL , L2PROJ
+    !
+    ! Defining the gmvoutputfolder
+    integer :: gmvfolder, checkneg
+    !
+    ! Variables for the defect correction ( nonlinear loop )
+    real(DP) :: defect , defectTol , negthres
+    !
+    ! Some params used to adjust the model
+    real(DP) :: CHI, D_1, D_2, A_CHEMO , A_CELLS , B_CHEMO , B_CELLS, W, SIGMA, BETA, ALPHA, R,  PHI, GAMMA, N
+    !
+    ! If INITSOL = 0 , meaning const init sol, we use these variables to
+    ! define the const value
+    real(DP) :: C_0, U_0
+    !
+    ! Scaling params for the analytic given sols
+    real(DP) :: SCALE_U , SCALE_C
+    !
+    ! Time step size, number of timesteps.(also for a fixed amount of steps, since there is
+    ! a basic loop control implemented dealing w/ the steady state analysis)
+    real(DP) :: dtstep
+    integer :: ntimesteps, steps
+    !
+    ! Time and time step counter
+    real(DP) :: dtime
+    integer :: itimestep
+    !
+    ! maximal iterations for the defect correction . Since we allow possible nonlinearities
+    integer :: maxiterationdef
+    !
+    ! error-norm constant
+    ! To be set in the .dat file
+    integer :: CTRLNORM
+    !
+    ! error analysis
+    real(DP) :: uerror, cerror, tol
+
+
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    !!!!! Ok, let's start. !!!!!
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    call parlst_init(rparams)
+
+     call parlst_readfromfile(rparams, 'data/chemotaxis.dat')
+
+    ! Getting some general params for the pb
+    call parlst_getvalue_int (rparams, 'GENERAL', 'NLMAX', NLMAX, 7) 
+    call parlst_getvalue_int (rparams, 'GENERAL' , 'OUTPUT' , OUTPUT , 0)
+    call parlst_getvalue_int (rparams, 'GENERAL' , 'ISTEP_GMV' , ISTEP_GMV , 1)
+    call parlst_getvalue_int (rparams, 'GENERAL' , 'INITSOL' , INITSOL , 1)
+    call parlst_getvalue_int ( rparams, 'GENERAL' , 'L2PROJ' , L2PROJ , 0)
+    call parlst_getvalue_int(rparams, 'GENERAL', 'GMVFOLDER', gmvfolder, 0)
+    call parlst_getvalue_int(rparams, 'GENERAL', 'CHECKNEG', checkneg , 0)
+    call parlst_getvalue_double(rparams, 'GENERAL', 'DEFECTTOL', defectTol , 0.0000001_DP)
+    call parlst_getvalue_double(rparams, 'GENERAL', 'NEGTHRES', negthres , -0.1_DP)
+      
+    ! We set the chemotaxis and diffusion coefficient
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'CHI', CHI, 1.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'W', W, 1.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'BETA', BETA, 1.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'SIGMA', SIGMA, 1.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'ALPHA', ALPHA, 1.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'R', R, 1.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'D_1', D_1, 1.0_DP)
+    D_1 = 0.0625_DP ! This is the diffusioncoefficient in the paper
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'PHI', PHI, 1.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'GAMMA', GAMMA, 1.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'N', N, 1.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'D_2', D_2, 1.0_DP)
+    D_2 = 1.0_DP  ! This is the diffusioncoefficient in the paper
+    call parlst_getvalue_double (rparams, 'coefficients', 'A_CHEMO', A_CHEMO, 10.0_dp)
+    call parlst_getvalue_double (rparams, 'coefficients', 'A_CELLS', A_CELLS, 0.6_dp)
+    call parlst_getvalue_double (rparams, 'coefficients', 'B_CHEMO', B_CHEMO, 10.0_dp)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'B_CELLS', B_CELLS, 10.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'U_0', U_0, 2.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'C_0', C_0, 1.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'SCALE_U', SCALE_U, 2.0_DP)
+    call parlst_getvalue_double (rparams, 'COEFFICIENTS', 'SCALE_C', SCALE_C, 1.0_DP)
+    
+     ! Initialise time step size and number of timesteps
+    call parlst_getvalue_int (rparams, 'TIMESTEPPING', 'NTIMESTEPS', ntimesteps, 100)
+    call parlst_getvalue_double (rparams, 'TIMESTEPPING', 'DTSTEP', dtstep, 0.00001_DP)
+    call parlst_getvalue_int (rparams, 'TIMESTEPPING', 'STEPS', steps, 0)
+    call parlst_getvalue_int (rparams, 'TIMESTEPPING', 'MAXITERATIONDEF', maxiterationdef, 10)
+
+    ! We start at a certain time ...
+    call parlst_getvalue_double(rparams, 'TIMESTEPPING' , 'STARTTIME' , dtime , 0.0_DP)
+    
+    ! Get the errorctrl norm
+    call parlst_getvalue_int (rparams, 'NORM', 'CONTROLNORM', CTRLNORM, 2)
+
+    ! Get the tolerance threshold value for steady state issues
+    call parlst_getvalue_double (rparams, 'ERROR', 'TOL', tol, 0.0001_DP)
+    
+    
+    
+    
+    
+    ! Definitions of variables.
     print *,' 3D version responds ... '
     print *,''
     
