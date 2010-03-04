5fd25ffb8a5be465c3255097d467aac69396b048
Raphael Muenster <raphael.muenster@math.tu-dortmund.de>
2010-03-04 18:32:53 +0000

    New features        : new application that uses a cpp nurbs library, it compiles on windows, under linux it compiles using gcc, the other compilers still have a few problems... Problems solved     : Bugzilla-Bug-ID     : Additional comments :
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@6920 4b12f95d-9313-0410-afc9-bd26427041f1

---
+++ area51/cc2d_ParticleShapes/src/ccinitgeneralparameters.f90
@@ -0,0 +1,460 @@
+!##############################################################################
+!# ****************************************************************************
+!# <name> ccinitgeneralparameters </name>
+!# ****************************************************************************
+!#
+!# <purpose>
+!# This module contains basic initialisation routines for CC2D:
+!# Initialisation of the main structures with parameters:
+!#
+!# 1.) cc_getLogFiles
+!#     -> Get information for LOG files from DAT files.
+!#
+!# 2.) cc2d_getDAT
+!#     -> Read all DAT files.
+!#
+!# 3.) cc_initParameters
+!#     -> Init the problem structure with data from the INI/DAT files
+!#
+!# 4.) cc_doneParameters
+!#     -> Clean up the problem structure
+!#
+!# </purpose>
+!##############################################################################
+
+module ccinitgeneralparameters
+
+  use fsystem
+  use storage
+  use linearsolver
+  use boundary
+  use bilinearformevaluation
+  use linearformevaluation
+  use cubature
+  use matrixfilters
+  use vectorfilters
+  use bcassembly
+  use triangulation
+  use spatialdiscretisation
+  use coarsegridcorrection
+  use spdiscprojection
+  use nonlinearsolver
+  use paramlist
+  
+  use collection
+  use convection
+  use geometry  
+  use ccbasic
+  use ccnonstationary
+  
+  implicit none
+  
+contains
+
+  ! ***************************************************************************
+
+!<subroutine>
+
+subroutine cc_initParticleDescriptor(rPDescriptor)
+  
+!<description>
+  ! here we initialise a particle descriptor
+  ! with the values we want to use
+!</description>
+  
+!<inputoutput>
+  type(t_ParticleDescriptor), intent(inout) :: rPDescriptor
+!</inputoutput>
+
+!</subroutine>
+  ! locals
+  integer :: i
+  real(dp) :: dx,dy,drho,drad
+  
+  rPDescriptor%iparticles=1
+  
+  allocate(rPDescriptor%pparameters(4,rPDescriptor%iparticles))
+
+  drad = 0.25_dp
+  drho = 1.25_dp
+  dx = 1.0_dp
+  dy = 6.0_dp
+
+  rPDescriptor%ishape = GEOM_NURBS
+  
+!  drad = 0.125_dp
+!  drho = 1.05_dp
+!  dx = 1.00001_dp
+!  dy = 7.3_dp
+!
+!  rPDescriptor%pparameters(1,1)= dx
+!  rPDescriptor%pparameters(2,1)= dy
+!  rPDescriptor%pparameters(3,1)= drad
+!  rPDescriptor%pparameters(4,1)= drho
+
+!  rPDescriptor%pparameters(1,2)= 0.9999_dp
+!  rPDescriptor%pparameters(2,2)= 6.8_dp
+!  rPDescriptor%pparameters(3,2)= drad
+!  rPDescriptor%pparameters(4,2)= drho
+
+  rPDescriptor%sfilename="pre/bean2d_5.3dm"
+  
+  do i=1,rPDescriptor%iparticles
+    rPDescriptor%pparameters(1,i)= dx
+    rPDescriptor%pparameters(2,i)= dy
+    rPDescriptor%pparameters(3,i)= drad
+    rPDescriptor%pparameters(4,i)= drho
+    dx = dx + 2*drad + 0.2_dp
+    dy = dy ! + 0.5_dp
+  end do
+  
+end subroutine ! end cc_initParticleDescriptor
+
+! ***************************************************************************
+
+
+! ***************************************************************************
+  
+!<subroutine>
+!subroutine cc_initParticleDescriptor(rPDescriptor)
+!  
+!!<description>
+!  ! here we initialise a particle descriptor
+!  ! with the values we want to use
+!!</description>
+!  
+!!<inputoutput>
+!  type(t_ParticleDescriptor), intent(inout) :: rPDescriptor
+!!</inputoutput>
+!
+!!</subroutine>
+!  ! locals
+!  integer :: i,j,m,n
+!  real(dp) :: dx,dy,drho,drad
+!  real(dp) :: x,y,e,h,dia,rem,quot,alpha
+!  
+!  rPDescriptor%iparticles=1
+!  
+!  allocate(rPDescriptor%pparameters(4,rPDescriptor%iparticles))
+!  
+!!   drad = 0.05_dp
+!  drad = 0.025_dp
+!  drho = 1.25_dp
+!!   dx = 0.2_dp
+!!   dy = 0.2_dp
+!  dx = 1.0_dp
+!  dy = 1.0_dp
+!  
+!  n = rPDescriptor%iparticles
+!  dia = 2*drad
+!  e = 0.05_dp ! minimum distance between particles and wall
+!  h = .05_dp ! height to start
+!
+!  ! increment of x and y
+!  x = 0.0_dp + (drad + e) ! 0.0_dp is the left x-boundary value
+!  y = 0.5_dp - (drad + e + h) ! 6.0_dp is the upper y-boundary value
+!
+!  rPDescriptor%pparameters(1,1) = x
+!  rPDescriptor%pparameters(2,1) = y
+!
+!  m = 0
+!  do i = 2,n
+!    x = x + (dia+e)
+!    rPDescriptor%pparameters(1,i) = x
+!    rPDescriptor%pparameters(2,i) = y
+!    m = m + 1;
+!    if ((1.0_dp-(x+drad)) .lt. e) then ! 2.0_dp is the right x-boundary value
+!        exit
+!    end if
+!  end do
+!
+!!  rem = mod(n,m) ! remainder
+!!  quot = (n-rem)/m ! quotient
+!!  alpha = 1.0_dp ! starting increment for alternate line
+!!
+!!  do i = 2,quot
+!!    y = y -(dia+e)
+!!      do j = 1,m
+!!        rPDescriptor%pparameters(1,j + (i-1)*m) = &
+!!        rPDescriptor%pparameters(1,j) - (1.0_dp + (-1.0_dp)**(i))/2*(-alpha*e)
+!!!         rPDescriptor%pparameters(1,j + (i-1)*m) = &
+!!!         rPDescriptor%pparameters(1,j)
+!!        rPDescriptor%pparameters(2,j + (i-1)*m) = y
+!!      end do
+!!  end do
+!!
+!!  do i = 1,rem
+!!    rPDescriptor%pparameters(1,m*quot + i) = &
+!!    rPDescriptor%pparameters(1,i) - (1.0_dp + (-1.0_dp)**(quot+1))/2*(-alpha*e)
+!!!     rPDescriptor%pparameters(1,m*quot + i) = &
+!!!     rPDescriptor%pparameters(1,i);
+!!    rPDescriptor%pparameters(2,m*quot + i) = y - (dia + e)
+!!  end do
+!!  
+!!  do i=1,rPDescriptor%iparticles
+!!    rPDescriptor%pparameters(3,i)= drad
+!!    rPDescriptor%pparameters(4,i)= drho
+!!  end do
+!  
+!
+!  
+!end subroutine ! end cc_initParticleDescriptor
+
+! ***************************************************************************
+
+!<subroutine>
+
+  subroutine cc_getLogFiles (slogfile,serrorfile,sbenchlogfile)
+  
+!<description>
+  ! Temporarily reads the output DAT file to get the names of the output
+  ! files.
+!</description>
+
+!<output>
+  ! Name of the message log file.
+  character(LEN=*), intent(out) :: slogfile
+  
+  ! Name of the error log file.
+  character(LEN=*), intent(out) :: serrorfile
+
+  ! Name of the benchmark log file.
+  character(LEN=*), intent(out) :: sbenchlogfile
+!</output>
+
+!</subroutine>
+
+    type(t_parlist) :: rparlist
+    character(LEN=SYS_STRLEN) :: sstring,smaster
+    logical :: bexists
+
+    ! Init parameter list that accepts parameters for output files
+    call parlst_init (rparlist)
+    
+    ! Check if a command line parameter specifies the master.dat file.
+    call sys_getcommandLineArg(1,smaster,sdefault='./data/master.dat')
+
+    ! Read parameters that configure the output
+    inquire(file=smaster, exist=bexists)
+    
+    if (bexists) then
+      ! Read the master file. That either one contains all parameters or
+      ! contains references to subfiles with data.
+      call parlst_readfromfile (rparlist, smaster)
+    else
+      call parlst_readfromfile (rparlist, './data/output.dat')
+    end if
+    
+    ! Now the real initialisation of the output including log file stuff!
+    call parlst_getvalue_string (rparlist,'GENERALOUTPUT',&
+                                'smsgLog',sstring,'''''')
+    read(sstring,*) slogfile
+
+    call parlst_getvalue_string (rparlist,'GENERALOUTPUT',&
+                                'serrorLog',sstring,'''''')
+    read(sstring,*) serrorfile
+
+    call parlst_getvalue_string (rparlist,'GENERALOUTPUT',&
+                                'sbenchLog',sstring,'''''')
+    read(sstring,*) sbenchlogfile
+    
+    ! That temporary parameter list is not needed anymore.
+    call parlst_done (rparlist)
+    
+    end subroutine
+
+  ! ***************************************************************************
+
+!<subroutine>
+
+  subroutine cc2d_getDAT (rparamList)
+  
+!<description>
+  ! Reads in all DAT files into the parameter list rparlist
+!</description>
+
+!<inputoutput>
+  ! The parameter list where the values of the DAT files should be stored.
+  ! The structure must have been initialised, the parameters are just added
+  ! to the list.
+  type(t_parlist), intent(inout) :: rparamList
+!</inputoutput>
+
+!</subroutine>
+
+    logical :: bexists
+    character(LEN=SYS_STRLEN) :: smaster
+    
+    ! Check if a command line parameter specifies the master.dat file.
+    call sys_getcommandLineArg(1,smaster,sdefault='./data/master.dat')
+    
+    ! Read the file 'master.dat'.
+    ! If that does not exist, try to manually read files with parameters from a
+    ! couple of files.
+    inquire(file=smaster, exist=bexists)
+    
+    if (bexists) then
+      ! Read the master file. That either one contains all parameters or
+      ! contains references to subfiles with data.
+      call parlst_readfromfile (rparamList, smaster)
+    else
+      ! Each 'readfromfile' command adds the parameter of the specified file 
+      ! to the parameter list.
+      call parlst_readfromfile (rparamList, './data/discretisation.dat')
+      call parlst_readfromfile (rparamList, './data/linsol_cc2d.dat')
+      call parlst_readfromfile (rparamList, './data/nonlinsol_cc2d.dat')
+      call parlst_readfromfile (rparamList, './data/output.dat')
+      call parlst_readfromfile (rparamList, './data/paramtriang.dat')
+      call parlst_readfromfile (rparamList, './data/bdconditions.dat')
+      call parlst_readfromfile (rparamList, './data/timediscr.dat')
+      call parlst_readfromfile (rparamList, './data/postprocessing.dat')
+    end if
+  
+  end subroutine
+
+  ! ***************************************************************************
+
+!<subroutine>
+
+  subroutine cc_initOutput (rproblem)
+  
+!<description>
+  ! Initialises basic output settings based on the parameters in the DAT file.
+!</description>
+  
+!<inputoutput>
+  ! A problem structure saving problem-dependent information.
+  type(t_problem), intent(inout) :: rproblem
+!</inputoutput>
+
+!</subroutine>
+
+    ! Get the output level for the whole application -- during the
+    ! initialisation phase and during the rest of the program.
+    call parlst_getvalue_int (rproblem%rparamList,'GENERALOUTPUT',&
+                              'MSHOW_Initialisation',rproblem%MSHOW_Initialisation,2)
+
+    call parlst_getvalue_int (rproblem%rparamList,'GENERALOUTPUT',&
+                              'MT_OutputLevel',rproblem%MT_OutputLevel,2)
+
+  end subroutine
+
+  ! ***************************************************************************
+
+!<subroutine>
+
+  subroutine cc_initParameters (rproblem)
+  
+!<description>
+  ! Initialises the structure rproblem with data from the initialisation
+  ! files.
+  !
+  ! The parameters in rproblem\%rparameters are evaluated.
+  ! Important parameters are written to the problem structure
+  ! rproblem and/or the enclosed collection rproblem\%rcollection.
+!</description>
+  
+!<inputoutput>
+  ! A problem structure saving problem-dependent information.
+  type(t_problem), intent(inout) :: rproblem
+!</inputoutput>
+
+!</subroutine>
+
+    real(DP) :: dnu
+    integer :: ilvmin,ilvmax,i1
+
+    ! Get the output level for the whole application -- during the
+    ! initialisation phase and during the rest of the program.
+    call parlst_getvalue_int (rproblem%rparamList,'GENERALOUTPUT',&
+                              'MSHOW_Initialisation',rproblem%MSHOW_Initialisation,2)
+
+    call parlst_getvalue_int (rproblem%rparamList,'GENERALOUTPUT',&
+                              'MT_OutputLevel',rproblem%MT_OutputLevel,2)
+
+    ! Get the viscosity model
+    ! Standard = 0 = constant viscosity
+    call parlst_getvalue_int (rproblem%rparamList,'CC-DISCRETISATION',&
+                                 'cviscoModel',rproblem%cviscoModel,0)
+    call parlst_getvalue_double (rproblem%rparamList,'CC-DISCRETISATION',&
+                                 'dviscoexponent',rproblem%dviscoexponent,2.0_DP)
+    call parlst_getvalue_double (rproblem%rparamList,'CC-DISCRETISATION',&
+                                 'dviscoEps',rproblem%dviscoEps,0.01_DP)
+
+    ! Get the viscosity parameter, save it to the problem structure
+    ! as well as into the collection.
+    ! Note that the parameter in the DAT file is 1/nu !
+    call parlst_getvalue_double (rproblem%rparamList,'CC-DISCRETISATION',&
+                                 'RE',dnu,1000.0_DP)
+
+    dnu = 1.0_DP/dnu
+    rproblem%dnu = dnu
+    
+    ! Get min/max level from the parameter file.
+    !
+    ! ilvmin receives the minimal level where to discretise for supporting
+    ! the solution process.
+    ! ilvmax receives the level where we want to solve.
+    
+    call parlst_getvalue_int (rproblem%rparamList,'CC-DISCRETISATION',&
+                              'NLMIN',ilvmin,2)
+    call parlst_getvalue_int (rproblem%rparamList,'CC-DISCRETISATION',&
+                              'NLMAX',ilvmax,4)
+
+    ! Initialise the level in the problem structure
+    if(ilvmin .le. 0) then
+      rproblem%NLMIN = max(1,ilvmax+ilvmin)
+    else
+      rproblem%NLMIN = ilvmin
+    end if
+    
+    rproblem%NLMAX = ilvmax
+    
+    ! Allocate memory for all the levels.
+    allocate(rproblem%RlevelInfo(1:ilvmax))
+
+    ! Which type of problem to discretise? (Stokes, Navier-Stokes,...)
+    call parlst_getvalue_int (rproblem%rparamList,'CC-DISCRETISATION',&
+                              'iEquation',i1,0)
+    rproblem%iequation = i1
+
+    ! Type of subproblem (gradient tensor, deformation tensor,...)
+    call parlst_getvalue_int (rproblem%rparamList,'CC-DISCRETISATION',&
+                              'isubEquation',i1,0)
+    rproblem%isubEquation = i1
+
+    ! Type of boundary conditions
+    call parlst_getvalue_int (rproblem%rparamList,'CC-DISCRETISATION',&
+                              'iBoundary',rproblem%iboundary,0)
+
+    ! Time dependence
+    call cc_initParTimeDependence (rproblem,'TIME-DISCRETISATION',&
+        rproblem%rparamList)
+
+  end subroutine
+
+  ! ***************************************************************************
+
+!<subroutine>
+
+  subroutine cc_doneParameters (rproblem)
+  
+!<description>
+  ! Cleans up parameters read from the DAT files. Removes all references to
+  ! parameters from the collection rproblem\%rcollection that were
+  ! set up in cc_initParameters.
+!</description>
+  
+!<inputoutput>
+  ! A problem structure saving problem-dependent information.
+  type(t_problem), intent(inout) :: rproblem
+!</inputoutput>
+
+!</subroutine>
+
+    ! Deallocate memory
+    deallocate(rproblem%RlevelInfo)
+
+  end subroutine
+
+end module
---
+++ area51/cc2d_ParticleShapes/src/ccinitparamtriang.f90
@@ -0,0 +1,243 @@
+!##############################################################################
+!# ****************************************************************************
+!# <name> ccinitparamtriang </name>
+!# ****************************************************************************
+!#
+!# <purpose>
+!# This module contains basic initialisation routines for CC2D:
+!# Initialisation of the parametrisation and the triangulations on all levels.
+!#
+!# 1.) cc_initParamTriang
+!#     -> Read parametrisation, read triangulation, refine the mesh
+!#
+!# 2.) cc_doneParamTriang
+!#     -> Remove the meshes of all levels from the heap
+!#
+!# </purpose>
+!##############################################################################
+
+module ccinitparamtriang
+
+  use fsystem
+  use storage
+  use linearsolver
+  use boundary
+  use bilinearformevaluation
+  use linearformevaluation
+  use cubature
+  use matrixfilters
+  use vectorfilters
+  use bcassembly
+  use triangulation
+  use spatialdiscretisation
+  use coarsegridcorrection
+  use spdiscprojection
+  use nonlinearsolver
+  use paramlist
+  use statistics
+  
+  use collection
+  use convection
+    
+  use ccbasic
+  
+  implicit none
+  
+contains
+
+  ! ***************************************************************************
+
+!<subroutine>
+
+  subroutine cc_initParamTriang (rproblem)
+  
+!<description>
+  ! This routine initialises the parametrisation and triangulation of the
+  ! domain. The corresponding .prm/.tri files are read from disc and
+  ! the triangulation is refined as described by the NLMIN/NLMAX parameters
+  ! from the INI/DAT files.
+!</description>
+
+!<inputoutput>
+  ! A problem structure saving problem-dependent information.
+  type(t_problem), intent(inout) :: rproblem
+!</inputoutput>
+
+!</subroutine>
+
+  ! local variables
+  integer :: i,ilvmin,ilvmax,nmdb
+  
+  ! Variable for a filename:  
+  character(LEN=SYS_STRLEN) :: sString
+  character(LEN=SYS_STRLEN) :: sPRMFile, sTRIFile
+  type(t_timer) :: rtimer
+  ! make all the regions
+  type(t_boundaryRegion), dimension(:),pointer :: p_rregion
+  
+  real(dp), dimension(:,:), pointer :: p_DbdyEdg      
+  
+  real(DP) :: dx,dy
+  integer :: iregions,iend,iindex   
+  integer, dimension(2) :: Isize        
+
+  call stat_clearTimer(rtimer)
+  call stat_startTimer(rtimer)
+
+  ! Get min/max level from the parameter file.
+  !
+  ! ilvmin receives the minimal level where to discretise for supporting
+  ! the solution process.
+  ! ilvmax receives the level where we want to solve.
+    
+  call parlst_getvalue_int (rproblem%rparamList,'CC-DISCRETISATION',&
+                            'NLMIN',ilvmin,2)
+  call parlst_getvalue_int (rproblem%rparamList,'CC-DISCRETISATION',&
+                            'NLMAX',ilvmax,4)
+
+  ! Get the .prm and the .tri file from the parameter list.
+  ! note that parlst_getvalue_string returns us exactly what stands
+  ! in the parameter file, so we have to apply READ to get rid of
+  ! probable ''!
+  call parlst_getvalue_string (rproblem%rparamList,'PARAMTRIANG',&
+                               'sParametrisation',sString)
+  read (sString,*) sPRMFile
+                            
+  call parlst_getvalue_string (rproblem%rparamList,'PARAMTRIANG',&
+                               'sMesh',sString)
+  read (sString,*) sTRIFile
+
+  ! Read in the parametrisation of the boundary and save it to rboundary.
+  call boundary_read_prm(rproblem%rboundary, sPrmFile)
+      
+  ! Now read in the basic triangulation.
+  call tria_readTriFile2D (rproblem%RlevelInfo(rproblem%NLMIN)%rtriangulation, &
+      sTRIFile, rproblem%rboundary)
+      
+  !---------------------------------------------------------------------------------        
+      
+  call tria_initStandardMeshFromRaw (&
+      rproblem%RlevelInfo(rproblem%NLMIN)%rtriangulation,rproblem%rboundary)
+      
+  ! get the number of boundary segments in
+  ! the boundary component 
+  iend = boundary_igetNsegments(rproblem%rboundary,1)
+
+  rproblem%isegments=iend
+
+  allocate(p_rregion(iend)) 
+
+  ! create the boundary regions
+  do iregions=1,iend
+  ! Idea: create the regions, check in which region the parameter is
+  call boundary_createRegion(rproblem%rboundary, 1,&
+                           iregions,p_rregion(iregions))
+  end do
+      
+
+  nmdb=rproblem%RlevelInfo(rproblem%NLMIN)%rtriangulation%NMBD
+
+  Isize = (/2,2*iend/)
+  
+  call storage_new ('cc_initParamTriang', 'edgesBdy',&
+      Isize, ST_DOUBLE, &
+      rproblem%h_DedgesAtBoundary, ST_NEWBLOCK_NOINIT)  
+  
+  call storage_getbase_double2D(rproblem%h_DedgesAtBoundary,p_DbdyEdg)
+
+  ! create the boundary regions
+  do iregions=1,iend
+
+    ! get the x,y coordinates for the current parameter value      
+    call boundary_getCoords(rproblem%rboundary, 1,&
+                            p_rregion(iregions)%dminParam, dx, dy)
+          
+    iindex=iregions*2-1 
+    p_DbdyEdg(1,iindex) = dx                       
+    p_DbdyEdg(2,iindex) = dy                       
+                                    
+    ! get the x,y coordinates for the current parameter value      
+    call boundary_getCoords(rproblem%rboundary, 1,&
+                            p_rregion(iregions)%dmaxParam, dx, dy)
+
+    p_DbdyEdg(1,iindex+1) = dx                       
+    p_DbdyEdg(2,iindex+1) = dy                       
+                        
+  end do
+                        
+  deallocate(p_rregion)        
+
+  !-----------------------------------------------------------------------------------------------    
+
+  ! Refine the mesh up to the minimum level
+  call tria_quickRefine2LevelOrdering(rproblem%NLMIN-1,&
+      rproblem%RlevelInfo(rproblem%NLMIN)%rtriangulation,rproblem%rboundary)
+
+  ! Create information about adjacencies and everything one needs from
+  ! a triangulation. Afterwards, we have the coarse mesh.
+  call tria_initStandardMeshFromRaw (&
+      rproblem%RlevelInfo(rproblem%NLMIN)%rtriangulation,rproblem%rboundary)
+
+  ! Now, refine to level up to nlmax.
+  do i=rproblem%NLMIN+1,rproblem%NLMAX
+    call tria_refine2LevelOrdering (rproblem%RlevelInfo(i-1)%rtriangulation,&
+        rproblem%RlevelInfo(i)%rtriangulation, rproblem%rboundary)
+    call tria_initStandardMeshFromRaw (rproblem%RlevelInfo(i)%rtriangulation,&
+        rproblem%rboundary)
+  end do
+
+  ! Compress the level hierarchy.
+  ! Share the vertex coordinates of all levels, so the coarse grid coordinates
+  ! are 'contained' in the fine grid coordinates. The effect is:
+  ! 1.) Save some memory
+  ! 2.) Every change in the fine grid coordinates also affects the coarse
+  !     grid coordinates and vice versa.
+  do i=rproblem%NLMAX-1,rproblem%NLMIN,-1
+    call tria_compress2LevelOrdHierarchy (rproblem%RlevelInfo(i+1)%rtriangulation,&
+        rproblem%RlevelInfo(i)%rtriangulation)
+  end do
+
+  ! Gather statistics
+  call stat_stopTimer(rtimer)
+  rproblem%rstatistics%dtimeGridGeneration = &
+    rproblem%rstatistics%dtimeGridGeneration + rtimer%delapsedReal
+    
+  !    call storage_new ('tria_genEdgesAtBoundary2D', 'KMBD', &
+  !          rproblem%RlevelInfo(1)%rtriangulation%NVBD, ST_INT, &
+  !          rproblem%RlevelInfo(1)%rtriangulation%h_IedgesAtBoundary, ST_NEWBLOCK_NOINIT)
+          
+                          
+
+  end subroutine
+
+  ! ***************************************************************************
+
+!<subroutine>
+
+  subroutine cc_doneParamTriang (rproblem)
+  
+!<description>
+  ! Releases the triangulation and parametrisation from the heap.
+!</description>
+
+!<inputoutput>
+  ! A problem structure saving problem-dependent information.
+  type(t_problem), intent(inout), target :: rproblem
+!</inputoutput>
+
+!</subroutine>
+
+  ! local variables
+  integer :: i
+
+    ! Release the triangulation on all levels
+    do i=rproblem%NLMAX,rproblem%NLMIN,-1
+      call tria_done (rproblem%RlevelInfo(i)%rtriangulation)
+    end do
+    
+    ! Finally release the domain.
+    call boundary_release (rproblem%rboundary)
+    
+  end subroutine
+
+end module
