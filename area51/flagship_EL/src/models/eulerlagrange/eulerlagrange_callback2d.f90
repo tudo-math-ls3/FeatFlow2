848b6cb7d04057fbf952d5443ff0250bb1bc518d
Nikolas Vogt <nikolas.vogt@math.tu-dortmund.de>
2010-08-10 13:06:01 +0000

    New features        : Euler-Lagrange application Problems solved     : Bugzilla-Bug-ID     : Additional comments : backup
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@7593 4b12f95d-9313-0410-afc9-bd26427041f1

--- area51/flagship_EL/src/models/eulerlagrange/eulerlagrange_callback2d.f90
+++ area51/flagship_EL/src/models/eulerlagrange/eulerlagrange_callback2d.f90
@@ -6327,16 +6327,16 @@ contains
 
                 ! Compute the dragforce
                 ! F_D = C_W * \frac{\pi}{8} * \rho_g * \d_p^2 * v_{rel} * \vec{v}_{rel}
-                F_D(1)= C_W*c_pi*rho_g*rParticles%p_diam(iPart)**2*Velo_rel*&
-                        (rParticles%p_xvelo_gas(iPart)-rParticles%p_xvelo_old(iPart))/8.0_dp
-                F_D(2)= C_W*c_pi*rho_g*rParticles%p_diam(iPart)**2*Velo_rel*&
-                        (rParticles%p_yvelo_gas(iPart)-rParticles%p_yvelo_old(iPart))/8.0_dp
-                !F_D(1)= (3*rho_g*rParticles%p_mass(iPart)*C_W*Velo_rel)/&
-                !        (4*rParticles%p_diam(iPart)*rParticles%p_density(iPart))*&
-                !        (rParticles%p_xvelo_gas(iPart)-rParticles%p_xvelo_old(iPart))
-                !F_D(2)= (3*rho_g*rParticles%p_mass(iPart)*C_W*Velo_rel)/&
-                !        (4*rParticles%p_diam(iPart)*rParticles%p_density(iPart))*&
-                !        (rParticles%p_yvelo_gas(iPart)-rParticles%p_yvelo_old(iPart))
+                !F_D(1)= C_W*c_pi*rho_g*rParticles%p_diam(iPart)**2*Velo_rel*&
+                !        (rParticles%p_xvelo_gas(iPart)-rParticles%p_xvelo_old(iPart))/8.0_dp
+                !F_D(2)= C_W*c_pi*rho_g*rParticles%p_diam(iPart)**2*Velo_rel*&
+                !        (rParticles%p_yvelo_gas(iPart)-rParticles%p_yvelo_old(iPart))/8.0_dp
+                F_D(1)= (3*rho_g*C_W*Velo_rel*rParticles%p_mass(iPart))/&
+                        (4*rParticles%p_diam(iPart)*rParticles%p_density(iPart))*&
+                        (rParticles%p_xvelo_gas(iPart)-rParticles%p_xvelo_old(iPart))
+                F_D(2)= (3*rho_g*C_W*Velo_rel*rParticles%p_mass(iPart))/&
+                        (4*rParticles%p_diam(iPart)*rParticles%p_density(iPart))*&
+                        (rParticles%p_yvelo_gas(iPart)-rParticles%p_yvelo_old(iPart))
 
             elseif (trim(cforce) .eq. 'gravity') then
 
@@ -6379,7 +6379,7 @@ contains
         end do ! Loop over all forces
 
         ! Set resulting force for the particle
-        F_ges(1)= F_D(1) + F_G(1) + F_VM(1) + F_B(1) + F_M(1) + F_S(1)
+        F_ges(1)= F_D(1) + F_G(1) + F_VM(1) + F_B(1) + F_M(1) + F_S(1) 
         F_ges(2)= F_D(2) + F_G(2) + F_VM(2) + F_B(2) + F_M(2) + F_S(2)
 
         ! Compute new velocity of the particle
