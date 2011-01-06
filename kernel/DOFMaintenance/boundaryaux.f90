6a8fe380a84e1788a26bf53e46fada54a33304d7
Matthias Moeller <matthias.moeller@math.tu-dortmund.de>
2011-01-06 12:39:05 +0000

    ENH: Added routine which gets the elements at one boundary component
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@8395 4b12f95d-9313-0410-afc9-bd26427041f1

--- kernel/DOFMaintenance/boundaryaux.f90
+++ kernel/DOFMaintenance/boundaryaux.f90
@@ -14,15 +14,22 @@
 !# 2.) bdraux_getElementsAtRegion
 !#     -> Calculates the list of elements at a boundary region.
 !#
-!# 3.) bdraux_getNELAtBoundary
+!# 3.) bdraux_getElementsAtBdrComp
+!#     -> Calculates the list of elements at a boundary component.
+!#
+!# 4.) bdraux_getNELAtBoundary
 !#     -> Calculates the number of element at the boundary
 !#
-!# 4.) bdraux_getNELAtRegion
+!# 5.) bdraux_getNELAtRegion
 !#     -> Calculates the number of element at a boundary region.
+!#
+!# 6.) bdraux_getNELAtBdrComp
+!#     -> Calculates the number of element at a boundary component.
 !# </purpose>
 !##############################################################################
 module boundaryaux
   
+  use basicgeometry
   use boundary
   use fsystem
   use genoutput
@@ -36,8 +43,10 @@ module boundaryaux
 
   public :: bdraux_getElementsAtBoundary
   public :: bdraux_getElementsAtRegion
+  public :: bdraux_getElementsAtBdrComp
   public :: bdraux_getNELAtBoundary
   public :: bdraux_getNELAtRegion
+  public :: bdraux_getNELAtBdrComp
   
 contains
 
@@ -379,6 +388,178 @@ contains
 
   !****************************************************************************
 
+!<subroutine>
+
+  subroutine bdraux_getElementsAtBdrComp(ibdc, rdiscretisation,&
+      NELbdc, IelementList, IelementOrientation, DedgePosition, celement)
+
+!<description>
+    ! This subroutine calculates the list of elements which are
+    ! adjacent to the boundary component ibdrComp. If the number of
+    ! elements exceeds the size of the working arrays an error is
+    ! thrown.
+!</description>
+
+!<input>
+    ! Number of the boundary component
+    integer, intent(in) :: ibdc
+
+    ! A discretisation structure
+    type(t_spatialdiscretisation), intent(in) :: rdiscretisation
+
+    ! OPTIOANL: Element type to be considered. If not present, then
+    ! all elements adjacent to the boundary are inserted into the list
+    integer(I32), intent(in), optional :: celement
+!</input>
+
+!<output>
+    ! The number of elements at the boundary region
+    integer, intent(out) :: NELbdc
+
+    ! The list of elements adjacent to the boundary region
+    integer, dimension(:), intent(out) :: IelementList
+
+    ! The orientation of elements adjacent to the boundary region
+    integer, dimension(:), intent(out) :: IelementOrientation
+
+    ! OPTIONAL: The start- and end-parameter values of the edges on
+    ! the boundary region
+    real(DP), dimension(:,:), intent(out), optional :: DedgePosition
+!</output>
+!</subroutine>
+
+    ! local variables
+    type(t_elementDistribution), dimension(:), pointer :: p_RelementDistribution
+    type(t_boundaryRegion) :: rboundaryRegion
+    type(t_boundary), pointer :: p_rboundary
+    type(t_triangulation), pointer :: p_rtriangulation
+    integer, dimension(:), pointer :: p_IboundaryCpIdx
+    integer, dimension(:), pointer :: p_IelementsAtBoundary
+    integer, dimension(:), pointer :: p_IelementDistr
+    integer, dimension(:), pointer :: p_InodalProperty
+    integer, dimension(:,:), pointer :: p_IverticesAtElement
+    integer :: iel,iedge
+
+    select case(rdiscretisation%ndimension)
+    case(NDIM1D)
+      ! Get some pointers and arrays for quicker access
+      p_rtriangulation => rdiscretisation%p_rtriangulation
+      p_RelementDistribution => rdiscretisation%RelementDistr
+
+      call storage_getbase_int (p_rtriangulation%h_IboundaryCpIdx,&
+          p_IboundaryCpIdx)
+      call storage_getbase_int (p_rtriangulation%h_IelementsAtBoundary,&
+          p_IelementsAtBoundary)
+      call storage_getbase_int2d (p_rtriangulation%h_IverticesAtElement,&
+          p_IverticesAtElement)
+      call storage_getbase_int (p_rtriangulation%h_InodalProperty,&
+        p_InodalProperty)
+
+      ! Number of elements on that boundary component?
+      NELbdc = p_IboundaryCpIdx(ibdc+1)-p_IboundaryCpIdx(ibdc)
+
+      if (NELbdc .gt. ubound(IelementList,1) .or.&
+          NELbdc .gt. ubound(IelementOrientation,1)) then
+        call output_line('Insufficient memory',&
+            OU_CLASS_ERROR,OU_MODE_STD,'bdraux_getElementsAtBdrComp')
+        call sys_halt()
+      end if
+
+      ! Do we have a uniform discretisation? Would simplify a lot...
+      if (rdiscretisation%ccomplexity.eq. SPDISC_UNIFORM) then
+        
+        ! Check if there are elements which satisfy the type of element
+        if (present(celement)) then
+          if (p_RelementDistribution(1)%celement .ne. celement) then
+            NELBdc = 0
+            return
+          end if
+        end if
+
+        ! Determine the element numbers and their orientation at the boundary
+        iel = 0
+        do iedge = p_IboundaryCpIdx(ibdc),p_IboundaryCpIdx(ibdc+1)-1
+          
+          iel = iel+1
+          
+          ! Element number
+          IelementList(iel) = p_IelementsAtBoundary(iedge)
+          
+          ! Element orientation, i.e. the local number of the boundary vertex
+          if (p_InodalProperty(&
+              p_IverticesAtElement(1,IelementList(iel))).eq. ibdc) then
+            IelementOrientation(iel) = 1
+          elseif (p_InodalProperty(&
+              p_IverticesAtElement(2,IelementList(iel))).eq. ibdc) then
+            IelementOrientation(iel) = 2
+          else
+            call output_line('Unable to determine element orientation!',&
+                OU_CLASS_ERROR,OU_MODE_STD,'bdraux_getElementsAtBdrComp')
+          end if
+        end do
+        
+      else
+
+        ! Set pointer
+        call storage_getbase_int (rdiscretisation%h_IelementDistr,&
+            p_IelementDistr)
+        
+        ! Determine the element numbers and their orientation at the boundary
+        iel = 0
+        do iedge = p_IboundaryCpIdx(ibdc),p_IboundaryCpIdx(ibdc+1)-1
+
+          ! Check if we are the correct element distribution
+          if (present(celement)) then
+            if (celement .ne. p_RelementDistribution(&
+                p_IelementDistr(p_IelementsAtBoundary(iedge)))%celement) cycle
+          end if
+          iel = iel+1
+          
+          ! Element number
+          IelementList(iel) = p_IelementsAtBoundary(iedge)
+          
+          ! Element orientation, i.e. the local number of the boundary vertex
+          if (p_InodalProperty(&
+              p_IverticesAtElement(1,IelementList(iel))).eq. ibdc) then
+            IelementOrientation(iel) = 1
+          elseif (p_InodalProperty(&
+              p_IverticesAtElement(2,IelementList(iel))).eq. ibdc) then
+            IelementOrientation(iel) = 2
+          else
+            call output_line('Unable to determine element orientation!',&
+                OU_CLASS_ERROR,OU_MODE_STD,'bdraux_getElementsAtBdrComp')
+          end if
+        end do
+        
+      end if
+      
+
+    case(NDIM2D)
+      ! The discretisation must provide a boundary structure
+      if (associated(rdiscretisation%p_rboundary)) then
+        p_rboundary => rdiscretisation%p_rboundary
+      else
+        call output_line('Discretisation does not provide boundary structure!',&
+            OU_CLASS_ERROR,OU_MODE_STD,'bdraux_getElementsAtBdrComp')
+        call sys_halt()
+      end if
+      
+      ! Create region for entire boundary and determine elements
+      call boundary_createRegion (p_rboundary, ibdc, 0, rboundaryRegion)
+      call bdraux_getElementsAtRegion(rboundaryRegion, rdiscretisation,&
+          NELbdc, IelementList, IelementOrientation, DedgePosition, celement)
+
+
+    case default
+      call output_line('Unsupported spatial dimension!',&
+          OU_CLASS_ERROR,OU_MODE_STD,'bdraux_getElementsAtBdrComp')
+      call sys_halt()
+    end select
+
+  end subroutine bdraux_getElementsAtBdrComp
+
+  !****************************************************************************
+
 !<function>
 
   function bdraux_getNELAtBoundary(rboundary, rtriangulation) result (NELbdc)
@@ -403,16 +584,13 @@ contains
 !</function>
 
     ! local variables
-    type(t_boundaryRegion) :: rboundaryRegion
     integer :: ibdc
     
     NELbdc = 0
     ! Create a boundary region for each boundary component and call
     ! the calculation routine for that.
     do ibdc = 1,boundary_igetNBoundComp(rboundary)
-      call boundary_createRegion (rboundary, ibdc, 0, rboundaryRegion)
-      NELbdc = NELbdc+&
-          bdraux_getNELAtRegion(rboundaryRegion, rtriangulation)
+      NELbdc = NELbdc+bdraux_getNELAtBdrComp(ibdc, rtriangulation)
     end do
     
   end function bdraux_getNELAtBoundary
@@ -457,5 +635,41 @@ contains
     NELbdc = p_IboundaryCpIdx(ibdc+1)-p_IboundaryCpIdx(ibdc)
     
   end function bdraux_getNELAtRegion
+
+  !****************************************************************************
+
+!<function>
+
+  function bdraux_getNELAtBdrComp(ibdc, rtriangulation) result(NELbdc)
+
+!<description>
+    ! This function calculates the number of elements which are
+    ! adjacent to the boundary component using the given triangulation.
+!</description>
+
+!<input>
+    ! Number of the boundary component where to calculate
+    integer, intent(in) :: ibdc
+
+    ! The triangulation structure
+    type(t_triangulation), intent(in) :: rtriangulation
+!</input>
+
+!<result>
+    ! The number of elements at the boundary region
+    integer :: NELbdc
+!</result>
+!</function>
+
+    ! local variables
+    integer, dimension(:), pointer :: p_IboundaryCpIdx
+
+    ! Set pointer
+    call storage_getbase_int (rtriangulation%h_IboundaryCpIdx, p_IboundaryCpIdx)
+    
+    ! Number of elements on that boundary component?
+    NELbdc = p_IboundaryCpIdx(ibdc+1)-p_IboundaryCpIdx(ibdc)
+    
+  end function bdraux_getNELAtBdrComp
   
 end module boundaryaux
