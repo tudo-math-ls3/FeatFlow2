04f4e8899b2b01cecb61de98857b0bd1456fe325
Michael Koester <michael.koester@math.tu-dortmund.de>
2012-06-18 10:29:56 +0000

    New features        : - Added some comments.
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@10370 4b12f95d-9313-0410-afc9-bd26427041f1

--- librariesNEW/bin/getfile_win.cmd
+++ librariesNEW/bin/getfile_win.cmd
@@ -3,6 +3,11 @@
 rem Executed in the BIN/ subdirectory.
 rem
 rem The script downloads a .tar.gz / .tgz file and extracts it.
+rem
+rem The script uses the Unix/Linux commands WGET, GZIP and TAR which are
+rem the original binaries provided by the following Web-Page:
+rem
+rem     http://gnuwin32.sourceforge.net/
 
 .\bin\wget %1 -O %~nx1
 .\bin\gzip -f --stdout -d %~nx1 | .\bin\tar -xf -
