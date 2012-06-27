104633b189a7fbea73fdd5ea493879ef9e47e2ce
Peter Zajac <peter.zajac@math.tu-dortmund.de>
2012-06-27 10:30:21 +0000

    ADDED: batch files for auto-build of new libraries for Visual Studio
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@10414 4b12f95d-9313-0410-afc9-bd26427041f1

---
+++ librariesNEW/bin/buildlib_win.cmd
@@ -0,0 +1,64 @@
+@echo off
+
+rem Build script for Visual Studio
+rem ------------------------------
+rem This file is a batch script that will compile one or all library/-ies
+rem using the Visual Studio development environment.
+rem
+rem Script Parameters:
+rem ------------------
+rem  %1: build configuration; either "Debug" or "Release"
+rem  %2: target platform; either "Win32" or "Win64"
+rem  %3: optional: project name; if not given all projects are compiled
+
+rem ===========================================================================
+
+set DEVENV=""
+
+rem Test for Visual Studio 2010
+if "%VS100COMNTOOLS%" neq "" set DEVENV="%VS100COMNTOOLS%..\IDE\devenv.exe"
+
+rem Ensure that we have a path to devenv.exe
+if %DEVENV% == "" goto novs
+
+rem ===========================================================================
+
+rem Check if the third parameter is given
+if "%3" == "" goto libsall
+
+
+rem Compile a single project from the solution
+
+echo Compiling '%3' in '%1' mode for '%2'...
+
+%DEVENV% .\visual_studio\libraries.sln /build "%1|%2" /project %3
+if %errorlevel% == 0 goto end
+
+echo.
+echo ERROR: Failed to compile '%3' in '%1' Mode for '%2' !
+goto end
+
+rem ===========================================================================
+:libsall
+
+rem Compile all projects from the solution
+
+echo Compiling all libraries in '%1' mode for '%2'...
+
+%DEVENV% .\visual_studio\libraries.sln /build "%1|%2"
+if %errorlevel% == 0 goto end
+
+echo.
+echo ERROR: Failed to compile libraries in '%1' Mode for '%2' !
+goto end
+
+rem ===========================================================================
+:novs
+
+echo.
+echo ERROR: No compatible Visual Studio installation found
+echo.
+goto end
+
+rem ===========================================================================
+:end
---
+++ librariesNEW/buildlibs_win64.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+call .\bin\buildlib_win.cmd Debug Win64
+
+call .\bin\buildlib_win.cmd Release Win64
