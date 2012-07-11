cd08a7b0aaa0eb4d1e3dae08f28fd78834b7637a
Peter Zajac <peter.zajac@math.tu-dortmund.de>
2012-07-11 17:21:11 +0000

    CHANGED: disabled full debug info for BLAS and LAPACK libraries in Release mode CHANGED: splib is not downloaded anymore for Windows, as it is unused
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@10494 4b12f95d-9313-0410-afc9-bd26427041f1

--- librariesNEW/getlibs_win.cmd
+++ librariesNEW/getlibs_win.cmd
@@ -10,6 +10,5 @@ rem the library/-ies and extract them.
 call .\AMD\getfiles_win.cmd
 call .\BLAS\getfiles_win.cmd
 call .\lapack-3.4.1\getfiles_win.cmd
-call .\splib\getfiles_win.cmd
 call .\SuiteSparse_config\getfiles_win.cmd
 call .\UMFPACK\getfiles_win.cmd
