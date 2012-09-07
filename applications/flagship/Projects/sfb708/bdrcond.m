2a928c19b6022613c5b57212a25101535ab756c0
Matthias Moeller <matthias.moeller@math.tu-dortmund.de>
2012-09-07 10:33:33 +0000

    BUG: Fixed wrong boundary data for SFB 708 Laval nozzle.
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@10673 4b12f95d-9313-0410-afc9-bd26427041f1

--- applications/flagship/Projects/sfb708/bdrcond.m
+++ applications/flagship/Projects/sfb708/bdrcond.m
@@ -1,19 +1,58 @@
-p_in = 5.1  ; % [bar]
+% Pressure and mass flow rate at the inlet
+p_in = 510000 ; % [Pa]
 m_in = 0.039; % [kg/s]
-p_out = 1.0 ; % [bar]
 
-Temp = 25; % [C°]
+% Area at the inlet
 Area = pi*0.0055^2; % [m^2]
+
+% Pressure at the outlet
+p_0  = 100000 ; % [Pa]
+
+% Temperature and gas constant
+Temp = 25; % [C°]
 gamma = 1.4;
 
+% --- Physical quantities ---
+disp('Physical quantities')
+
 % Density [kg/m^3]
-rho_in = (p_in*1e5) / (287.058 * (Temp + 273.15))
-%      bar*kg/(m*s^2) / J/(kg*K) * K
+rho_in = (p_in) / (287.058 * (Temp + 273.15))
+rho_0  = (p_0) / (287.058 * (Temp + 273.15))
 
 % Velocity in x-direction [m/s]
-vx_in = m_in/(rho_in*A)
+vx_in = m_in/(rho_in*Area)
 vy_in = 0
-%    m^3/(h*bar)
+
+% Pressure
+p_in
+p_0
 
 % Energy
-E = (p_in*1e5) / ((gamma-1)*rho_in) + 0.5*sqrt(vx_in^2+vy_in^2)
+E_in = (p_in) / (gamma-1) + 0.5*rho_in*(vx_in^2+vy_in^2)
+E_0  = (p_0) / (gamma-1)
+
+
+% Reference values
+rho_r = rho_in
+vel_r = vx_in
+len_r = 1.0
+
+
+% --- Dimensionless quantities ---
+disp('Dimensionless quantities')
+
+% Density
+rho_in = rho_in/rho_r
+rho_0  = rho_0/rho_r
+
+% Velocity
+vx_in = vx_in/vel_r
+vy_in = vy_in/vel_r
+
+% Pressure
+p_in = p_in/(rho_r*vel_r^2)
+p_0  = p_0/(rho_r*vel_r^2)
+
+% Energy
+E_in = E_in/(rho_r*vel_r^2)
+E_0  = E_0/(rho_r*vel_r^2)
