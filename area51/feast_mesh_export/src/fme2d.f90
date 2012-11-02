ad57674566436b5b43a57243f88f48a8e748b03d
Peter Zajac <peter.zajac@math.tu-dortmund.de>
2012-11-02 11:39:47 +0000

    added chart-file writer to FEAST mesh exporter
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@10870 4b12f95d-9313-0410-afc9-bd26427041f1

--- area51/feast_mesh_export/src/fme2d.f90
+++ area51/feast_mesh_export/src/fme2d.f90
@@ -20,20 +20,18 @@ contains
 !<subroutine>
 
   subroutine fme_2d(rparam)
-  type(t_parlist), intent(INOUT) :: rparam
+  type(t_parlist), intent(inout) :: rparam
 
 !</subroutine>
 
   type(t_boundary) :: rbnd
   type(t_triangulation) :: rtria
-  integer :: nLevel, iunit, ibct, nlines, i
-  character(len=SYS_STRLEN) :: sPrmFile, sTriFile, sMeshFile, sMeshPath
-  character(len=256) :: sInfoLine
+  integer :: nLevel
+  character(len=SYS_STRLEN) :: sPrmFile, sTriFile, sMeshFile, sChartFile
   
     ! fetch parameters
     call parlst_getvalue_string(rparam, '', 'SPRMFILE', sPrmFile, '')
     call parlst_getvalue_string(rparam, '', 'STRIFILE', sTriFile, '')
-    call parlst_getvalue_string(rparam, '', 'SMESHFILE', sMeshFile, '')
     call parlst_getvalue_int(rparam, '', 'NLEVEL', nLevel, 0)
 
     ! Read in parametrisation
@@ -45,9 +43,213 @@ contains
     ! Refine up to desired level
     call tria_quickRefine2LevelOrdering (nLevel, rtria, rbnd)
     call tria_initStandardMeshFromRaw (rtria, rbnd)
+    
+    ! write chart file
+    if(parlst_queryvalue(rparam, '', 'SCHARTFILE') .gt. 0) then
+      call parlst_getvalue_string(rparam, '', 'SCHARTFILE', sChartFile, '')
+      call fma2d_writeChartFile('./out/' // trim(sChartFile), rbnd, rparam)
+    end if
+
+    ! write mesh file
+    if(parlst_queryvalue(rparam, '', 'SMESHFILE') .gt. 0) then
+      call parlst_getvalue_string(rparam, '', 'SMESHFILE', sMeshFile, '')
+      call fma2d_writeMeshFile('./out/' // trim(sMeshFile), rtria, rbnd, rparam)
+    end if
+
+    ! Clean up the mess
+    call tria_done (rtria)
+    call boundary_release (rbnd)
+
+  end subroutine
+
+  ! ***********************************************************************************************
+
+  subroutine fma2d_writeChartFile(sChartPath, rbnd, rparam)
+  character(len=*), intent(in) :: sChartPath
+  type(t_boundary), intent(inout) :: rbnd
+  type(t_parlist), intent(inout) :: rparam
+
+  character(len=256) :: sInfoLine
+  integer :: iunit, nlines, i, ibct
+  type(t_boundaryRegion) :: rrgn
 
     ! open file for writing
-    sMeshPath = './out/' // trim(sMeshFile) // '.txt'
+    call io_openFileForWriting(trim(sChartPath), iunit, SYS_REPLACE) !, bformatted=.true.)
+
+    ! Fail?
+    if(iunit .le. 0) then
+      call output_line('Failed to open "' // trim(sChartPath) // '" for writing!', &
+          OU_CLASS_ERROR, OU_MODE_STD)
+      call sys_halt()
+    end if
+    
+    ! write file starter
+    write(iunit,'(A)') '<feast_chart_file>'
+    
+    ! write file header
+    write(iunit,'(A)') '<header>'
+    write(iunit,'(A)') ' version 1'
+    write(iunit,'(A)') ' charts ' // trim(sys_sil(boundary_igetNBoundComp(rbnd),4))
+    write(iunit,'(A)') '</header>'
+    
+    ! write file info (if present)
+    nlines = parlst_querysubstrings(rparam, '', 'sChartFileInfo')
+    if(nlines .gt. 0) then
+      write(iunit,'(A)') '<info>'
+      do i=1, nlines
+        call parlst_getvalue_string (rparam, '', 'sChartFileInfo', sInfoLine, '', i)
+        write(iunit,'(A)') ' ' // trim(sInfoLine)
+      end do
+      write(iunit,'(A)') '</info>'
+    end if
+
+    ! loop over all boundary components
+    do ibct = 1, boundary_igetNBoundComp(rbnd)
+    
+      ! check for uniformity of boundary component
+      call fma2d_checkBndComp(rbnd, ibct)
+    
+      ! write chart chunk
+      call boundary_createRegion(rbnd, ibct, 1, rrgn)
+      select case(rrgn%isegmentType)
+      case (BOUNDARY_TYPE_LINE)
+        call fma2d_writeChartPolyline(iunit, rbnd, ibct)
+        
+      case (BOUNDARY_TYPE_CIRCLE)
+        call fma2d_writeChartCircle(iunit, rbnd, ibct)
+      end select
+      
+    end do
+    
+    ! write file terminator
+    write(iunit,'(A)') '</feast_chart_file>'
+    
+    ! close output file
+    close(iunit)  
+
+  end subroutine
+  
+  ! ***********************************************************************************************
+
+  subroutine fma2d_checkBndComp(rbnd, ibct)
+  type(t_boundary), intent(inout) :: rbnd
+  integer, intent(in) :: ibct
+  
+  type(t_boundaryRegion) :: rrgn
+  integer :: iseg, nseg, ctype
+  
+    nseg = boundary_igetNsegments(rbnd, ibct)
+    call boundary_createRegion(rbnd, ibct, 1, rrgn)
+    ctype = rrgn%isegmentType
+    
+    do iseg=2, nseg
+      call boundary_createRegion(rbnd, ibct, iseg, rrgn)
+      if(ctype .ne. rrgn%isegmentType) then
+        call output_line('ERROR: boundary component ' // trim(sys_sil(ibct,4)) &
+          // ' has mixed segment types!', OU_CLASS_ERROR, OU_MODE_STD)
+        call sys_halt()
+      end if
+    end do
+
+  end subroutine
+
+  ! ***********************************************************************************************
+
+  subroutine fma2d_writeChartPolyline(iunit, rbnd, ibct)
+  integer, intent(in) :: iunit
+  type(t_boundary), intent(inout) :: rbnd
+  integer, intent(in) :: ibct
+  
+  integer :: nseg, i
+  real(DP) :: dx,dy
+  
+    ! fetch the segment count
+    nseg = boundary_igetNsegments(rbnd, ibct)
+    
+    ! write chart starter
+    write(iunit,'(A)') '<chart>'
+
+    ! write header
+    write(iunit,'(A)') ' <header>'
+    write(iunit,'(A)') '  name ' // trim(sys_sil(ibct,8))
+    write(iunit,'(A)') '  type polyline'
+    write(iunit,'(A)') '  img_dim 2'
+    write(iunit,'(A)') ' </header>'
+    
+    ! write polyline chunk
+    write(iunit,'(A)') ' <polyline>'
+    write(iunit,'(A)') '  num_points ' // trim(sys_sil(nseg+1,8))
+    write(iunit,'(A)') '  <points>'
+    do i=0, nseg
+      call boundary_getCoords(rbnd, ibct, real(i,DP), dx, dy)
+      write(iunit,'(A)') '   ' // trim(sys_sil(i,8)) // ' ' // &
+        trim(fmtCoord(dx)) // ' ' // trim(fmtCoord(dy))
+    end do
+    write(iunit,'(A)') '  </points>'
+    write(iunit,'(A)') ' </polyline>'
+    
+    ! write chart terminator
+    write(iunit,'(A)') '</chart>'
+
+  end subroutine
+
+  ! ***********************************************************************************************
+
+  subroutine fma2d_writeChartCircle(iunit, rbnd, ibct)
+  integer, intent(in) :: iunit
+  type(t_boundary), intent(inout) :: rbnd
+  integer, intent(in) :: ibct
+  
+  real(DP) :: dz, dx0,dy0, dx1, dy1, dr
+  
+    ! fetch maximum parameter value
+    dz = boundary_dgetMaxParVal(rbnd, ibct)
+    
+    ! fetch point coords
+    call boundary_getCoords(rbnd, ibct, 0.0_DP, dx0, dy0)
+    call boundary_getCoords(rbnd, ibct, 0.5_DP*dz, dx1, dy1)
+    
+    ! compute radius
+    dr = 0.5_DP * sqrt((dx1-dx0)**2 + (dy1-dy0)**2)
+  
+    ! write chart starter
+    write(iunit,'(A)') '<chart>'
+
+    ! write header
+    write(iunit,'(A)') ' <header>'
+    write(iunit,'(A)') '  name ' // trim(sys_sil(ibct,8))
+    write(iunit,'(A)') '  type circle'
+    write(iunit,'(A)') ' </header>'
+    
+    ! write circle chunk
+    write(iunit,'(A)') ' <circle>'
+    write(iunit,'(A)') '  radius ' // trim(fmtCoord(dr))
+    write(iunit,'(A)') '  midpoint ' // &
+      trim(fmtCoord(0.5_DP*(dx0+dx1))) // ' ' // &
+      trim(fmtCoord(0.5_DP*(dy0+dy1)))
+    write(iunit,'(A)') '  domain 0 ' // trim(fmtCoord(dz))
+    write(iunit,'(A)') ' </circle>'
+    
+    ! write circle terminator
+    write(iunit,'(A)') '</chart>'
+
+  end subroutine
+  
+  ! ***********************************************************************************************
+
+  subroutine fma2d_writeMeshFile(sMeshPath, rtria, rbnd, rparam)
+  character(len=*), intent(in) :: sMeshPath
+  type(t_triangulation), intent(inout) :: rtria
+  type(t_boundary), intent(inout) :: rbnd
+  type(t_parlist), intent(inout) :: rparam
+
+  character(len=256) :: sInfoLine, sChartFile
+  integer :: iunit, nlines, i, ibct
+  logical :: bhaveChart
+  
+    bhaveChart = .false.
+
+    ! open file for writing
     call io_openFileForWriting(sMeshPath, iunit, SYS_REPLACE) !, bformatted=.true.)
 
     ! Fail?
@@ -63,22 +265,50 @@ contains
     ! write file header
     write(iunit,'(A)') '<header>'
     write(iunit,'(A)') ' version 1'
+    if(parlst_queryvalue(rparam, '', 'SCHARTFILE') .gt. 0) then
+      ! write chart file name
+      call parlst_getvalue_string(rparam, '', 'SCHARTFILE', sChartFile, '')
+      write(iunit,'(A)') ' chart_file ' // trim(sChartFile)
+      bhaveChart = .true.
+    end if
     write(iunit,'(A)') ' submeshes ' // trim(sys_sil(rtria%NBCT,4))
     write(iunit,'(A)') ' cellsets 0'
     write(iunit,'(A)') '</header>'
     
     ! write file info (if present)
-    nlines = parlst_querysubstrings(rparam, '', 'sFileInfo')
+    nlines = parlst_querysubstrings(rparam, '', 'sMeshFileInfo')
     if(nlines .gt. 0) then
       write(iunit,'(A)') '<info>'
       do i=1, nlines
-        call parlst_getvalue_string (rparam, '', 'sFileInfo', sInfoLine, '', i)
+        call parlst_getvalue_string (rparam, '', 'sMeshFileInfo', sInfoLine, '', i)
         write(iunit,'(A)') ' ' // trim(sInfoLine)
       end do
       write(iunit,'(A)') '</info>'
     end if
     
+    ! write mesh chunk
+    call fma2d_writeMeshChunk(iunit, rtria, rbnd)
     
+    ! write submeshes
+    do ibct = 1, rtria%NBCT
+      call fma2d_writeSubmeshChunk(iunit, rtria, rbnd, ibct, bhaveChart)
+    end do
+    
+    ! write file terminator
+    write(iunit,'(A)') '</feast_mesh_file>'
+    
+    ! close output file
+    close(iunit)  
+  
+  end subroutine
+
+  ! ***********************************************************************************************
+
+  subroutine fma2d_writeMeshChunk(iunit, rtria, rbnd)
+  integer, intent(in) :: iunit
+  type(t_triangulation), intent(inout) :: rtria
+  type(t_boundary), intent(inout) :: rbnd
+  
     ! write mesh starter
     write(iunit,'(A)') '<mesh>'
     
@@ -121,28 +351,16 @@ contains
     ! write mesh terminator
     write(iunit,'(A)') '</mesh>'
     
-    ! write submeshes
-    do ibct = 1, rtria%NBCT
-      call fma2d_writeSubmeshChunk(iunit, rtria, rbnd, ibct)
-    end do
-    
-    ! write file terminator
-    write(iunit,'(A)') '</feast_mesh_file>'
-    
-    ! close output file
-    close(iunit)
-
-    ! Clean up the mess
-    call tria_done (rtria)
-    call boundary_release (rbnd)
-
   end subroutine
+  
+  ! ***********************************************************************************************
 
-  subroutine fma2d_writeSubmeshChunk(iunit, rtria, rbnd, ibct)
+  subroutine fma2d_writeSubmeshChunk(iunit, rtria, rbnd, ibct, bhaveChart)
   integer, intent(in) :: iunit
   type(t_triangulation), intent(inout) :: rtria
   type(t_boundary), intent(inout) :: rbnd
   integer, intent(in) :: ibct
+  logical, intent(in) :: bhaveChart
   
   integer :: i, n0, n1
   integer, dimension(:), pointer :: p_IbdCptIdx, p_Ivtx, p_Iedge
@@ -165,6 +383,8 @@ contains
     write(iunit,'(A)') ' <header>'
     write(iunit,'(A)') '  name ' // trim(sys_sil(ibct,8))
     write(iunit,'(A)') '  parent root'
+    if(bhaveChart) &
+      write(iunit,'(A)') '  chart ' // trim(sys_sil(ibct,8))
     write(iunit,'(A)') '  type conformal'
     write(iunit,'(A)') '  shape edge'
     write(iunit,'(A)') '  coords 1'
