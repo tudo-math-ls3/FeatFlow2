e7c61cea50aae65eb98be4c995450c97e2e83539
Masoud Nickaeen <masoud.nickaeen@math.tu-dortmund.de>
2013-06-13 22:10:59 +0000

    vvp-defaults
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@11404 4b12f95d-9313-0410-afc9-bd26427041f1

--- area51/lsfem/src/LS_NS_VVP_MG2D.f90
+++ area51/lsfem/src/LS_NS_VVP_MG2D.f90
@@ -416,7 +416,7 @@ contains
   
   ! Physical scaling
   if (scPhysic .eq. 1) then
-    rcollection%DquickAccess(2) = 1.0_DP/(dnu) !20.0_DP !
+    rcollection%DquickAccess(2) = 20.0_DP !1.0_DP/(dnu) !
   else
     rcollection%DquickAccess(2) = 1.0_DP
   end if
