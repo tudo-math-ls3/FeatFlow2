f41d7b65cec78106823abafce7b2a0ce1c844f5c
Matthias Moeller <matthias.moeller@math.tu-dortmund.de>
2013-06-25 15:00:42 +0000

    ENH: Updated UMFPACK 5.6.1 to 5.6.2,              SuiteSparse_config 4.0.2 to 4.2.0
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@11436 4b12f95d-9313-0410-afc9-bd26427041f1

--- librariesNEW/UMFPACK/getfiles_win.cmd
+++ librariesNEW/UMFPACK/getfiles_win.cmd
@@ -2,6 +2,6 @@
 
 rem Download and extract the file
 
-set URL=http://www.cise.ufl.edu/research/sparse/umfpack/UMFPACK-5.6.1.tar.gz
+set URL=http://www.cise.ufl.edu/research/sparse/umfpack/UMFPACK-5.6.2.tar.gz
 
 call .\bin\getfile_win.cmd %URL%
