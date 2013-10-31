44dd96f9eec4a1492f0bb0dff2f26fa10986f264
Matthias Moeller <matthias.moeller@math.tu-dortmund.de>
2013-10-31 18:47:24 +0000

    BUG: Fixed error introduced by commit 11707
    git-svn-id: svn://doncamillo.mathematik.tu-dortmund.de/Featflow2@11711 4b12f95d-9313-0410-afc9-bd26427041f1

--- kernel/LinearSystem/linearsystemscalar.f90
+++ kernel/LinearSystem/linearsystemscalar.f90
@@ -6782,6 +6782,11 @@ contains
   integer :: i,j,nrows,ncols,ivar,jvar
   integer :: h_Da,h_Kcol,h_Kld,h_Kdiagonal
 
+#ifdef USE_INTEL_MKL
+  integer, dimension(6) :: Ijob
+  integer :: info
+#endif
+
   ! Matrix is already in that format.
   if (rmatrix%cmatrixFormat .eq. cmatrixFormat) return
 
@@ -7330,33 +7335,21 @@ contains
           call lsyssc_getbase_double(rmatrix, p_Ddata)
           call storage_getbase_double(ihandle, p_Ddata2)
 
-#ifdef USE_INTEL_MKL
-          Ijob = (/1,1,1,2,rmatrix%NA,1/)
-          call mkl_ddnscsr(Ijob,rmatrix%NEQ,rmatrix%NCOLS,p_Ddata,&
-              rmatrix%NEQ,p_Ddata2,p_Kcol,p_Kld,info)
-#else
           do iEQ = 1,rmatrix%NEQ
             do iA = p_Kld(ieq), p_Kld(ieq+1)-1
               p_Ddata2(rmatrix%NCOLS*(iEQ-1)+p_Kcol(iA)) = p_Ddata(iA)
             end do
           end do
-#endif
 
         case (ST_SINGLE)
           call lsyssc_getbase_single(rmatrix, p_Fdata)
           call storage_getbase_single(ihandle, p_Fdata2)
 
-#ifdef USE_INTEL_MKL
-          Ijob = (/1,1,1,2,rmatrix%NA,1/)
-          call mkl_sdnscsr(Ijob,rmatrix%NEQ,rmatrix%NCOLS,p_Ddata,&
-              rmatrix%NEQ,p_Ddata2,p_Kcol,p_Kld,info)
-#else
           do iEQ = 1,rmatrix%NEQ
             do iA = p_Kld(ieq), p_Kld(ieq+1)
               p_Fdata2(rmatrix%NCOLS*(iEQ-1)+p_Kcol(iA)) = p_Fdata(iA)
             end do
           end do
-#endif
 
         case default
           call output_line('Unsupported data type!',&
