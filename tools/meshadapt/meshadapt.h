ebb1bce99cdfccef604cdff37c74f6d444004293
Matthias Moeller <matthias.moeller@math.tu-dortmund.de>
2014-07-08 20:03:19 +0000

    ENH: Prototypic MATLAB-binding for meshadapt.
    git-svn-id: https://svn.mathematik.tu-dortmund.de:20443/svn/Featflow2@11903 4f116ff2-4e46-254c-a92c-0a7323bc4f3a

---
+++ tools/meshadapt/meshadapt.h
@@ -0,0 +1,41 @@
+/* This header file is meant to load the shared library
+ * libmeshadapt.so into Matlab and to call the functions and
+ * subroutines from the Featflow2 kernel and the mesh adaptation
+ * applications from Matlab.
+ *
+ * NOTE: This header file only provides access to a limited number of
+ *       functions and subroutine and sometimes omits optional
+ *       arguments or makes them mandatory.
+ */
+
+#include "types.h"
+
+struct t_meshAdapt {
+  struct t_boundary* rboundary;
+  struct t_triangulation rtriangulation;
+  struct t_hadapt rhadapt;
+  int nrefmax;
+  double dreftol;
+  double dcrstol;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  // fsystem.f90
+  void __fsystem_MOD_sys_init_simple();
+  
+  // genoutput.f90
+  void __genoutput_MOD_output_init_simple();
+  
+  // storage.f90
+  void __storage_MOD_storage_done(void*);
+  void __storage_MOD_storage_init(int*, int*, void*);
+
+  // meshadaptbase.f90
+  void __meshadaptbase_MOD_madapt_init(int*, char*, void*);
+  void __meshadaptbase_MOD_madapt_done(void*);
+  
+#ifdef __cplusplus
+} /* end extern "C" */
+#endif
